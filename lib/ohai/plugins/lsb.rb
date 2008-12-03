3cf7f59ad70b72bf5efe8e8fce6262e6e1b0d8fe
Adam Jacob <adam@hjksolutions.com>
2008-12-03 13:51:21 -0800

    Updaing lsb and memory specs, making us track lots more memory stats.

--- lib/ohai/plugins/lsb.rb
+++ lib/ohai/plugins/lsb.rb
@@ -16,19 +16,21 @@
 # limitations under the License.
 #
 
-begin
-  File.open("/etc/lsb-release").each do |line|
-    case line
-    when /^DISTRIB_ID=(.+)$/
-      lsb_dist_id $1
-    when /^DISTRIB_RELEASE=(.+)$/
-      lsb_dist_release $1
-    when /^DISTRIB_CODENAME=(.+)$/
-      lsb_dist_codename $1
-    when /^DISTRIB_DESCRIPTION=(.+)$/
-      lsb_dist_description $1
+if os == "linux"
+  begin
+    File.open("/etc/lsb-release").each do |line|
+      case line
+      when /^DISTRIB_ID=(.+)$/
+        lsb_dist_id $1
+      when /^DISTRIB_RELEASE=(.+)$/
+        lsb_dist_release $1
+      when /^DISTRIB_CODENAME=(.+)$/
+        lsb_dist_codename $1
+      when /^DISTRIB_DESCRIPTION=(.+)$/
+        lsb_dist_description $1
+      end
     end
+  rescue
+    Ohai::Log.debug("Skipping LSB, cannot find /etc/lsb-release")
   end
-rescue
-  Ohai::Log.debug("Skipping LSB, cannot find /etc/lsb-release")
 end

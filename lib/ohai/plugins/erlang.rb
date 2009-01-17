b6391f1e33132e2c0a7037219ec148c70fe0ebce
zeusfaber <joe@joetify.com>
2009-01-17 10:13:06 -0800

    slight change to make things a little shorter

--- lib/ohai/plugins/erlang.rb
+++ lib/ohai/plugins/erlang.rb
@@ -1,13 +1,12 @@
 require_plugin "languages"
 require "open3"
-
+ 
 languages[:erlang] = Mash.new
-
+ 
 stdin, stdout, stderr = Open3.popen3('erl +V')
+ 
+output = stderr.gets.split
 
-output = stderr.gets
-info = output.split
-
-languages[:erlang][:version] = info[5]
-languages[:erlang][:options] = info[1]
-languages[:erlang][:emulator] = info[2]
+languages[:erlang][:version] = output[5]
+languages[:erlang][:options] = output[1]
+languages[:erlang][:emulator] = output[2]

cb9068870b0837e6b6288f4dd8ca8eba37d3ace5
Bryan McLellan <bryanm@widemile.com>
2009-01-24 22:01:58 -0800

    identify freebsd as freebsd instead of freebsd7

--- lib/ohai/plugins/freebsd/platform.rb
+++ lib/ohai/plugins/freebsd/platform.rb
@@ -1,14 +1,14 @@
 #
-# Author:: Adam Jacob (<adam@opscode.com>)
-# Copyright:: Copyright (c) 2008 Opscode, Inc.
+# Author:: Bryan McLellan (btm@loftninjas.org)
+# Copyright:: Copyright (c) 2009 Bryan McLellan
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,19 +16,6 @@
 # limitations under the License.
 #
 
-require_plugin 'ruby'
-require_plugin 'kernel'
-
-case languages[:ruby][:host_os]
-when /darwin(.+)$/
-  os "darwin"
-when /linux/
-  os "linux"
-when /freebsd(.+)$/
- os "freebsd"
-else
-  os languages[:ruby][:host_os]
-end
-
-os_version kernel[:release]
+platform from("uname -s").downcase
+platform_version from("uname -r")
 

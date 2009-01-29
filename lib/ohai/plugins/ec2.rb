9d58c3264bfc41b20cbd686d65b217d5bc2ed304
Tim Dysinger <tim@dysinger.net>
2009-01-28 16:01:04 -1000

    finally got ec2 plugin working with good specs. resolves #ohai-33

--- lib/ohai/plugins/ec2.rb
+++ lib/ohai/plugins/ec2.rb
@@ -1,4 +1,5 @@
 #
+# Author:: Tim Dysinger (<tim@dysinger.net>)
 # Author:: Benjamin Black (<bb@opscode.com>)
 # Copyright:: Copyright (c) 2009 Opscode, Inc.
 # License:: Apache License, Version 2.0
@@ -7,15 +8,13 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# NOTICE - based on contributions from Tim Dysinger and Ezra Zygmuntowicz
 
 require 'open-uri'
 
@@ -23,12 +22,12 @@ require_plugin "hostname"
 require_plugin "kernel"
 
 def metadata(id='')
-  open("http://169.254.169.254/2008-02-01/meta-data/#{id}").read.
+  OpenURI.open_uri("http://169.254.169.254/2008-02-01/meta-data/#{id}").read.
     split("\n").each do |o|
     key = "#{id}#{o.gsub(/\=.*$/, '/')}"
     if key[-1..-1] != '/'
       ec2[key.gsub(/\-|\//, '_').to_sym] = begin
-        value = open("http://169.254.169.254/2008-02-01/meta-data/#{key}").
+        value = OpenURI.open_uri("http://169.254.169.254/2008-02-01/meta-data/#{key}").
           read.split("\n")
         value.size > 1 ? value : value.first
       end
@@ -38,9 +37,8 @@ def metadata(id='')
   end
 end
 
-if (domain =~ /\.amazonaws.com$/) ||
-   (domain =~ /\.internal$/) ||
-   (kernel[:version] =~ /-ec2-/)
+if (os == 'linux' &&
+    (domain =~ /\.internal$/ || kernel[:release] =~ /ec2/))
   ec2 Mash.new
   self.metadata
 end

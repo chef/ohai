83db276ab481385751065fd5ba288d07fac56acb
Bryan McLellan <btm@loftninjas.org>
2009-01-31 13:38:07 +0000

    add other virtualization types

--- lib/ohai/plugins/linux/virtualization.rb
+++ lib/ohai/plugins/linux/virtualization.rb
@@ -29,3 +29,48 @@ elsif File.exists?("/proc/sys/xen/independent_wallclock")
   virtualization[:system] = "xen"
   virtualization[:role] = "guest"
 end
+
+# Detect KVM hosts by kernel module
+if File.exists?("/proc/modules")
+  if File.read("/proc/modules") =~ /^kvm/
+    virtualization[:system] = "kvm"
+    virtualization[:role] = "host"
+  end
+end
+
+# Detect KVM/QEMU from cpuinfo, report as KVM
+# We could pick KVM from 'Booting paravirtualized kernel on KVM' in dmesg
+# 2.6.27-9-server (intrepid) has this / 2.6.18-6-amd64 (etch) does not
+# It would be great if we could read pv_info in the kernel
+# Wait for reply to: http://article.gmane.org/gmane.comp.emulators.kvm.devel/27885
+if File.exists?("/proc/cpuinfo")
+  if File.read("/proc/cpuinfo") =~ /QEMU Virtual CPU/
+    virtualization[:system] = "kvm"
+    virtualization[:role] = "guest"
+  end
+end
+
+# http://www.dmo.ca/blog/detecting-virtualization-on-linux
+if File.exists?("/usr/sbin/dmidecode")
+  popen4("dmidecode") do |pid, stdin, stdout, stderr|
+    stdin.close
+    stdout.each do |line|
+      case line
+      when /Manufacturer: Microsoft/
+        found_virt_manufacturer = "virtualpc"
+      when / Product Name: Virtual Machine/
+        if found_virt_manufacturer == "virtualpc" 
+          virtualization[:system] = "virtualpc"
+          virtualization[:role] = "guest"
+        end
+      when /Manufacturer: VMware/
+        found_virt_manufacturer = "vmware"
+      when /Product Name: VMware Virtual Platform/
+        if found_virt_manufacturer == "vmware" 
+          virtualization[:system] = "vmware"
+          virtualization[:role] = "guest"
+        end
+      end
+    end
+  end
+end

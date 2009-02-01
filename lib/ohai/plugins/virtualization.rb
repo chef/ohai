1ff3479c661996baa69b4322a3044f6fa5949a01
Benjamin Black <bb@joyeuse-2.local>
2009-01-31 21:31:57 -0800

    [OHAI-36] removed extraneous xml_desc foo

--- lib/ohai/plugins/virtualization.rb
+++ lib/ohai/plugins/virtualization.rb
@@ -21,34 +21,48 @@ require_plugin "#{os}::virtualization"
 unless virtualization.nil? || !(virtualization[:role].eql?("host"))
   require 'libvirt'
 
-  virtconn = Libvirt::open("#{(virtualization[:system].eql?('kvm') ? 'qemu' : virtualization[:system])}:///system")
+  system = (virtualization[:system].eql?('kvm') ? 'qemu' : virtualization[:system])
+  virtualization[:libvirt_version] = Libvirt::version(system)[0].to_s
+  
+  virtconn = Libvirt::open("#{system}:///system")
 
   virtualization[:uri] = virtconn.uri
   virtualization[:capabilities] = virtconn.capabilities
   virtualization[:nodeinfo] = Mash.new
-  # why doesn't the NodeInfo object respond to attributes?  argh.
-  ['cores','cpus','memory','mhz','model','nodes','sockets','threads'].each {|a| virtualization[:nodeinfo][a] = virtconn.node_get_info.send(a)}
+  ni = virtconn.node_get_info
+  ['cores','cpus','memory','mhz','model','nodes','sockets','threads'].each {|a| virtualization[:nodeinfo][a] = ni.send(a)}
 
   virtualization[:domains] = Mash.new
   virtconn.list_domains.each do |d|
-    virtualization[:domains][d] = virtconn.lookup_domain_by_id(d).info.attributes
-    virtualization[:domains][d]["xml_desc"] = virtconn.lookup_domain_by_id(d).xml_desc
+    dv = virtconn.lookup_domain_by_id(d)
+    virtualization[:domains][dv.name] = Mash.new
+    virtualization[:domains][dv.name][:id] = d
+    ['os_type','uuid','xml_spec'].each {|a| virtualization[:domains][dv.name][a] = dv.send(a)}
+    ['cpu_time','max_mem','memory','nr_virt_cpu','state'].each {|a| virtualization[:domains][dv.name][a] = dv.info.send(a)}
   end
   
   virtualization[:networks] = Mash.new
   virtconn.list_networks.each do |n|
+    nv = virtconn.lookup_network_by_name(n)
     virtualization[:networks][n] = Mash.new
-    virtualization[:networks][n]["xml_desc"] = virtconn.lookup_network_by_name(n).xml_desc
-    virtualization[:networks][n]["bridge"] = virtconn.lookup_network_by_name(n).bridge_name
+    virtualization[:networks][n][:bridge] = vn.bridge_name
+    virtualization[:networks][n][:xml_desc] = vn.xml_desc
   end
   
   virtualization[:storage] = Mash.new
-  virtconn.list_storage_pools.each do |pool| 
+  virtconn.list_storage_pools.each do |pool|
+    sp = virtconn.lookup_storage_pool_by_name(pool)
     virtualization[:storage][pool] = Mash.new
-    virtualization[:storage][pool][:info] = virtconn.lookup_storage_pool_by_name(pool).info.attributes
-  
+    ['autostart','uuid','xml_desc'].each {|a| virtualization[:storage][pool][a] = sp.send(a)}
+    ['allocation','available','capacity','state'].each {|a| virtualization[:storage][pool][a] = sp.info.send(a)}
+    
     virtualization[:storage][pool][:volumes] = Mash.new
-    virtconn.list_volumes.each {|v| virtualization[:storage][pool][:volumes][v] = virtconn.list_volume_by_name(pool).info.attributes}
+    sp.list_volumes.each do |v|
+      virtualization[:storage][pool][:volumes][v] = Mash.new
+      sv = virtconn.lookup_volume_by_name(pool)
+      ['key','name','path'].each {|a| virtualization[:storage][pool][:volumes][v][a] = sv.send(a)}
+      ['allocation','capacity','type'].each {|a| virtualization[:storage][pool][:volumes][v][a] = sv.info.send(a)}
+    end
   end
   
   virtconn.close

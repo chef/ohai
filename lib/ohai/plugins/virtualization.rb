b3743f7666c369c8693b5a6e94a510e3fcceb4e1
Benjamin Black <bb@joyeuse.local>
2009-02-02 14:46:01 -0800

    [OHAI-36] fixed typo in volume lookup

--- lib/ohai/plugins/virtualization.rb
+++ lib/ohai/plugins/virtualization.rb
@@ -70,7 +70,7 @@ unless virtualization.nil? || !(virtualization[:role].eql?("host"))
     virtualization[:storage][pool][:volumes] = Mash.new
     sp.list_volumes.each do |v|
       virtualization[:storage][pool][:volumes][v] = Mash.new
-      sv = virtconn.lookup_volume_by_name pool
+      sv = sp.lookup_volume_by_name pool
       ['key','name','path'].each {|a| virtualization[:storage][pool][:volumes][v][a] = sv.send(a)}
       ['allocation','capacity','type'].each {|a| virtualization[:storage][pool][:volumes][v][a] = sv.info.send(a)}
     end

d7ddb8951728bb952f20729f8e6982623d782d99
jtimberman <joshua@opscode.com>
2009-06-16 20:11:37 -0600

    ohai-91, check if /dev/mem is readable, the dmidecode version, and for version specific dmi strings

--- lib/ohai/plugins/dmi.rb
+++ lib/ohai/plugins/dmi.rb
@@ -20,38 +20,43 @@ provides "dmi"
 
 # dmidecode does not return data without access to /dev/mem (or its equivalent)
 
-dmi Mash.new
+if File.stat("/dev/mem").readable?
+  dmi Mash.new
+  dmi[:version] = from("dmidecode --version")
+  
+  dmi[:bios] = Mash.new
+  dmi[:bios][:vendor] =             from("dmidecode -s bios-vendor")
+  dmi[:bios][:version] =            from("dmidecode -s bios-version")
+  dmi[:bios][:release_date] =       from("dmidecode -s bios-release-date")
 
-dmi[:bios] = Mash.new
-dmi[:bios][:vendor] =             from("dmidecode -s bios-vendor")
-dmi[:bios][:version] =            from("dmidecode -s bios-version")
-dmi[:bios][:release_date] =       from("dmidecode -s bios-release-date")
+  dmi[:system] = Mash.new
+  dmi[:system][:manufacturer] =     from("dmidecode -s system-manufacturer")
+  dmi[:system][:product_name] =     from("dmidecode -s system-product-name")
+  dmi[:system][:version] =          from("dmidecode -s system-version")
+  dmi[:system][:serial_number] =    from("dmidecode -s system-serial-number")
 
-dmi[:system] = Mash.new
-dmi[:system][:manufacturer] =     from("dmidecode -s system-manufacturer")
-dmi[:system][:product_name] =     from("dmidecode -s system-product-name")
-dmi[:system][:version] =          from("dmidecode -s system-version")
-dmi[:system][:serial_number] =    from("dmidecode -s system-serial-number")
-dmi[:system][:uuid] =             from("dmidecode -s system-uuid")
+  dmi[:baseboard] = Mash.new
+  dmi[:baseboard][:manufacturer] =  from("dmidecode -s baseboard-manufacturer")
+  dmi[:baseboard][:product_name] =  from("dmidecode -s baseboard-product-name")
+  dmi[:baseboard][:version] =       from("dmidecode -s baseboard-version")
+  dmi[:baseboard][:serial_number] = from("dmidecode -s baseboard-serial-number")
+  dmi[:baseboard][:asset_tag] =     from("dmidecode -s baseboard-asset-tag")
 
-dmi[:baseboard] = Mash.new
-dmi[:baseboard][:manufacturer] =  from("dmidecode -s baseboard-manufacturer")
-dmi[:baseboard][:product_name] =  from("dmidecode -s baseboard-product-name")
-dmi[:baseboard][:version] =       from("dmidecode -s baseboard-version")
-dmi[:baseboard][:serial_number] = from("dmidecode -s baseboard-serial-number")
-dmi[:baseboard][:asset_tag] =     from("dmidecode -s baseboard-asset-tag")
+  dmi[:chassis] = Mash.new
+  dmi[:chassis][:manufacturer] =    from("dmidecode -s chassis-manufacturer")
+  dmi[:chassis][:version] =         from("dmidecode -s chassis-version")
+  dmi[:chassis][:serial_number] =   from("dmidecode -s chassis-serial-number")
+  dmi[:chassis][:asset_tag] =       from("dmidecode -s chassis-asset-tag")
 
-dmi[:chassis] = Mash.new
-dmi[:chassis][:manufacturer] =    from("dmidecode -s chassis-manufacturer")
-dmi[:chassis][:type] =            from("dmidecode -s chassis-type")
-dmi[:chassis][:version] =         from("dmidecode -s chassis-version")
-dmi[:chassis][:serial_number] =   from("dmidecode -s chassis-serial-number")
-dmi[:chassis][:asset_tag] =       from("dmidecode -s chassis-asset-tag")
+  dmi[:processor] = Mash.new
+  dmi[:processor][:manufacturer] =  from("dmidecode -s processor-manufacturer")
+  dmi[:processor][:version] =       from("dmidecode -s processor-version")
 
-dmi[:processor] = Mash.new
-dmi[:processor][:family] =        from("dmidecode -s processor-family")
-dmi[:processor][:manufacturer] =  from("dmidecode -s processor-manufacturer")
-dmi[:processor][:version] =       from("dmidecode -s processor-version")
-dmi[:processor][:frequency] =     from("dmidecode -s processor-frequency")
+  if dmi[:version].to_f >= 2.8
+    dmi[:chassis][:type] =            from("dmidecode -s chassis-type")
+    dmi[:system][:uuid] =             from("dmidecode -s system-uuid") 
+    dmi[:processor][:family] =        from("dmidecode -s processor-family") 
+    dmi[:processor][:frequency] =     from("dmidecode -s processor-frequency")
+  end
 
-
+end

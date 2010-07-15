feb1c8d33ee68193e66a93800050830850af96df
Kurt Yoder <ktyopscode@yoderhome.com>
2010-07-15 16:55:47 -0400

    [OHAI-184] Pull maximal information from dmidecode, and fall back to smbios

--- lib/ohai/plugins/solaris2/dmi.rb
+++ lib/ohai/plugins/solaris2/dmi.rb
@@ -16,60 +16,16 @@
 # limitations under the License.
 #
 
-provides "dmi"
+require_plugin "dmi"
 
-# dmidecode does not return data without access to /dev/mem (or its equivalent)
+# if we already have a "dmi" with keys (presumably from dmidecode), don't try smbios
+# note that a single key just means dmidecode exited with its version
+if (dmi.class.to_s == 'Mash') and (dmi.keys.length > 1) 
+  Ohai::Log.info('skipping smbios output, since DMI information has already been provided')
+  return
+end
 
 dmi Mash.new
-
-# List of IDs and what they translate to
-# from 'man 8 dmidecode'
-id_to_description= {
-  0 =>   'bios',
-  1 =>   'system',
-  2 =>   'baseboard',
-  3 =>   'chassis',
-  4 =>   'processor',
-  5 =>   'memory_controller',
-  6 =>   'memory_module',
-  7 =>   'cache',
-  8 =>   'port_connector',
-  9 =>   'system_slots',
-  10 =>  'on_board_devices',
-  11 =>  'oem_strings',
-  12 =>  'system_configuration_options',
-  13 =>  'bios_language',
-  14 =>  'group_associations',
-  15 =>  'system_event_log',
-  16 =>  'physical_memory_array',
-  17 =>  'memory_device',
-  18 =>  '32_bit_memory_error',
-  19 =>  'memory_array_mapped_address',
-  20 =>  'memory_device_mapped_address',
-  21 =>  'built_in_pointing_device',
-  22 =>  'portable_battery',
-  23 =>  'system_reset',
-  24 =>  'hardware_security',
-  25 =>  'system_power_controls',
-  26 =>  'voltage_probe',
-  27 =>  'cooling_device',
-  28 =>  'temperature_probe',
-  29 =>  'electrical_current_probe',
-  30 =>  'out_of_band_remote_access',
-  31 =>  'boot_integrity_services',
-  32 =>  'system_boot',
-  33 =>  '64_bit_memory_error',
-  34 =>  'management_device',
-  35 =>  'management_device_component',
-  36 =>  'management_device_threshold_data',
-  37 =>  'memory_channel',
-  38 =>  'ipmi_device',
-  39 =>  'power_supply',
-  126 => 'disabled_entries',
-  127 => 'end_of_table_marker',
-}
-# 128-255 are 'OEM Data'
-# Everything else is unknown
 
 # bad Solaris shows strings defined by system instead of SMB IDs 
 # this is what the *real* IDs are:
@@ -150,30 +106,26 @@ popen4("smbios") do |pid, stdin, stdout, stderr|
   #         SMB_BIOSFL_PCI (PCI is supported)
   # ... similar lines trimmed
   # note the second level of indentation is via a *tab*
-  stdout.each do |line|
-    next if header_type_line.match(line)
-    next if blank_line.match(line)
+  stdout.each do |raw_line|
+    next if header_type_line.match(raw_line)
+    next if blank_line.match(raw_line)
+
+    # remove/replace any characters that don't fall inside permissible ASCII range, or whitespace
+    line = raw_line.gsub(/[^\x20-\x7E\n\t\r]/, '.')
+	 if (line != raw_line)
+		 Ohai::Log.debug("converted characters from line:\n#{raw_line}")
+    end
 
     if header_information = header_information_line.match(line)
       dmi_record = {}
 
       # look up SMB ID
       if smb_to_id.has_key?(header_information[3])
-        dmi_record[:type] = smb_to_id[header_information[3]]
-  
-        # look up DMI ID
-        if (dmi_record[:type] >= 128) and (dmi_record[:type] <= 255)
-          dmi_record[:type] = "oem_#{dmi_record[:type]}"
-        elsif id_to_description.has_key?(dmi_record[:type])
-          dmi_record[:type] = id_to_description[dmi_record[:type]]
-        else
-          dmi_record[:type] = 'unknown'
-          Ohai::Log.warn("unrecognized header id; falling back to 'unknown'")
-        end
+        dmi_record[:type] = DMI.id_lookup(smb_to_id[header_information[3]])
 
       else
         dmi_record[:type] = header_information[3].downcase
-        Ohai::Log.warn("unrecognized header type; falling back to #{dmi_record[:type]}")
+        Ohai::Log.info("unrecognized header type; falling back to #{dmi_record[:type]}")
       end
 
       dmi[dmi_record[:type]] = Mash.new unless dmi.has_key?(dmi_record[:type])
@@ -182,12 +134,12 @@ popen4("smbios") do |pid, stdin, stdout, stderr|
       dmi[dmi_record[:type]][:all_records].push(Mash.new)
       dmi[dmi_record[:type]][:all_records][dmi_record[:position]][:record_id] = header_information[1]
       dmi[dmi_record[:type]][:all_records][dmi_record[:position]][:size] = header_information[2]
-      dmi[dmi_record[:type]][:all_records][dmi_record[:position]][:os_identifier] = header_information[4]
+      dmi[dmi_record[:type]][:all_records][dmi_record[:position]][:application_identifier] = header_information[4]
       field = nil
     
     elsif data = data_key_value_line.match(line)
       if dmi_record == nil
-        Ohai::Log.warn("unexpected data line found before header; discarding:\n#{line}")
+        Ohai::Log.info("unexpected data line found before header; discarding:\n#{line}")
         next
       end
       dmi[dmi_record[:type]][:all_records][dmi_record[:position]][data[1]] = data[2]
@@ -195,7 +147,7 @@ popen4("smbios") do |pid, stdin, stdout, stderr|
     
     elsif data = data_key_only_line.match(line)
       if dmi_record == nil
-        Ohai::Log.warn("unexpected data line found before header; discarding:\n#{line}")
+        Ohai::Log.info("unexpected data line found before header; discarding:\n#{line}")
         next
       end
       dmi[dmi_record[:type]][:all_records][dmi_record[:position]][data[1]] = ''
@@ -203,11 +155,11 @@ popen4("smbios") do |pid, stdin, stdout, stderr|
     
     elsif extended_data = extended_data_line.match(line)
       if dmi_record == nil
-        Ohai::Log.warn("unexpected extended data line found before header; discarding:\n#{line}")
+        Ohai::Log.info("unexpected extended data line found before header; discarding:\n#{line}")
         next
       end
       if field == nil
-        Ohai::Log.warn("unexpected extended data line found outside data section; discarding:\n#{line}")
+        Ohai::Log.info("unexpected extended data line found outside data section; discarding:\n#{line}")
         next
       end
       # overwrite "raw" value with a new Mash
@@ -215,33 +167,10 @@ popen4("smbios") do |pid, stdin, stdout, stderr|
       dmi[dmi_record[:type]][:all_records][dmi_record[:position]][field][extended_data[1]] = extended_data[2]
 
     else
-      Ohai::Log.warn("unrecognized output line; discarding:\n#{line}")
+      Ohai::Log.info("unrecognized output line; discarding:\n#{line}")
 
     end
   end
 end
 
-# create simplified convenience access keys for each record type
-# for single occurrences of one type, copy to top level all fields and values
-# for multiple occurrences of same type, copy to top level all fields and values that are common to all records
-dmi.each{ |type, records|
-  in_common = Mash.new
-  records[:all_records].each{ |record| 
-    record.each{ |field, value| 
-      next if value.class.to_s == 'Mash'
-      next if field.to_s == 'os_identifier'
-      next if field.to_s == 'size'
-      next if field.to_s == 'record_id'
-      translated = field.downcase.gsub(/[^a-z0-9]/, '_')
-      if in_common.has_key?(translated)
-        in_common[translated] = nil unless in_common[translated] == value
-      else
-        in_common[translated] = value
-      end
-    }
-  }
-  in_common.each{ |field, value|
-    next if value == nil
-    dmi[type][field] = value
-  }
-}
+DMI.convenience_keys(dmi)

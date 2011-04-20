eaa4c773c2b4c68a961f639312fd8c307660a9f5
Seth Chisamore <schisamo@opscode.com>
2011-04-19 22:28:26 -0400

    [OHAI-265,OHAI-266] cloud plugins should have consistant attribute naming
    * rackspace plugin attributes should have consistent naming with the ec2 plugin
    * cloud plugin should expose additional meta data

--- lib/ohai/plugins/rackspace.rb
+++ lib/ohai/plugins/rackspace.rb
@@ -67,4 +67,8 @@ if looks_like_rackspace?
   rackspace Mash.new
   get_ip_address(:public_ip, :eth0)
   get_ip_address(:private_ip, :eth1)
+  rackspace[:public_ipv4] = rackspace[:public_ip]
+  rackspace[:public_hostname] = "#{rackspace[:public_ip].gsub('.','-')}.static.cloud-ips.com"
+  rackspace[:local_ipv4] = rackspace[:private_ip]
+  rackspace[:local_hostname] = hostname
 end

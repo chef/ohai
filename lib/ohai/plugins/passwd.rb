78f259e1e76cc65b4a4f5701113875a32d08cf01
Toomas Pelberg <toomas.pelberg@playtech.com>
2011-08-03 09:56:07 +0300

    Read only the local /etc/passwd /etc/group if exists - otherwise use Etc

--- lib/ohai/plugins/passwd.rb
+++ lib/ohai/plugins/passwd.rb
@@ -7,14 +7,29 @@ unless etc
 
   etc[:passwd] = Mash.new
   etc[:group] = Mash.new
+
+  if File.exists?("/etc/passwd") && File.exists?("/etc/group")
+    File.readlines("/etc/passwd").each do |line|
+      splitline = line.chomp.split(":")
+      etc[:passwd][splitline[0]] = Mash.new(:dir => splitline[5], :gid => splitline[2].to_i, :uid => splitline[3].to_i, :shell => splitline[6], :gecos => splitline[4])
+    end
   
-  Etc.passwd do |entry|
-    etc[:passwd][entry.name] = Mash.new(:dir => entry.dir, :gid => entry.gid, :uid => entry.uid, :shell => entry.shell, :gecos => entry.gecos)
+    File.readlines("/etc/group").each do |line|
+      splitline = line.chomp.split(":")
+      g_members = splitline[3..-1].join.split(",")
+      etc[:group][splitline[0]] = Mash.new(:gid => splitline[2].to_i, :members => g_members)
+    end
+    
+  else
+    Etc.passwd do |entry|
+      etc[:passwd][entry.name] = Mash.new(:dir => entry.dir, :gid => entry.gid, :uid => entry.uid, :shell => entry.shell, :gecos => entry.gecos)
+    end
+    
+    Etc.group do |entry|
+      etc[:group][entry.name] = Mash.new(:gid => entry.gid, :members => entry.mem)
+    end
   end
   
-  Etc.group do |entry|
-    etc[:group][entry.name] = Mash.new(:gid => entry.gid, :members => entry.mem)
-  end
 end
 
 unless current_user
--- spec/ohai/plugins/passwd_spec.rb
+++ spec/ohai/plugins/passwd_spec.rb
@@ -5,17 +5,6 @@ describe Ohai::System, "plugin etc" do
     @ohai = Ohai::System.new
     @ohai.stub!(:require_plugin).and_return(true)
   end
-
-  PasswdEntry = Struct.new(:name, :uid, :gid, :dir, :shell, :gecos)
-  GroupEntry = Struct.new(:name, :gid, :mem)
-
-  it "should include a list of all users" do
-    Etc.should_receive(:passwd).and_yield(PasswdEntry.new("root", 1, 1, '/root', '/bin/zsh', 'BOFH')).
-      and_yield(PasswdEntry.new('www', 800, 800, '/var/www', '/bin/false', 'Serving the web since 1970'))
-    @ohai._require_plugin("passwd")
-    @ohai[:etc][:passwd]['root'].should == Mash.new(:shell => '/bin/zsh', :gecos => 'BOFH', :gid => 1, :uid => 1, :dir => '/root')
-    @ohai[:etc][:passwd]['www'].should == Mash.new(:shell => '/bin/false', :gecos => 'Serving the web since 1970', :gid => 800, :uid => 800, :dir => '/var/www')
-  end
   
   it "should set the current user" do
     Etc.should_receive(:getlogin).and_return('chef')
@@ -23,10 +12,37 @@ describe Ohai::System, "plugin etc" do
     @ohai[:current_user].should == 'chef'
   end
   
-  it "should set the available groups" do
-    Etc.should_receive(:group).and_yield(GroupEntry.new("admin", 100, ['root', 'chef'])).and_yield(GroupEntry.new('www', 800, ['www', 'deploy']))
-    @ohai._require_plugin("passwd")
-    @ohai[:etc][:group]['admin'].should == Mash.new(:gid => 100, :members => ['root', 'chef'])
-    @ohai[:etc][:group]['www'].should == Mash.new(:gid => 800, :members => ['www', 'deploy'])
+  if File.exists?("/etc/passwd") && File.exists?("/etc/group")
+    it "should include a list of all users" do
+      File.should_receive(:readlines).exactly(2).times.and_return(["root:x:0:0:BOFH:/root:/bin/zsh","www:x:800:800:Serving the web since 1970:/var/www:/bin/false"])
+      @ohai._require_plugin("passwd")
+      @ohai[:etc][:passwd]['root'].should == Mash.new(:shell => '/bin/zsh', :gecos => 'BOFH', :gid => 0, :uid => 0, :dir => '/root')
+      @ohai[:etc][:passwd]['www'].should == Mash.new(:shell => '/bin/false', :gecos => 'Serving the web since 1970', :gid => 800, :uid => 800, :dir => '/var/www')
+    end
+    
+    it "should set the available groups" do
+      File.should_receive(:readlines).exactly(2).times.and_return(["admin:x:100:root,chef","www:x:800:www,deploy"])
+      @ohai._require_plugin("passwd")
+      @ohai[:etc][:group]['admin'].should == Mash.new(:gid => 100, :members => ['root', 'chef'])
+      @ohai[:etc][:group]['www'].should == Mash.new(:gid => 800, :members => ['www', 'deploy'])
+    end
+  else
+    PasswdEntry = Struct.new(:name, :uid, :gid, :dir, :shell, :gecos)
+    GroupEntry = Struct.new(:name, :gid, :mem)
+  
+    it "should include a list of all users" do
+      Etc.should_receive(:passwd).and_yield(PasswdEntry.new("root", 1, 1, '/root', '/bin/zsh', 'BOFH')).
+        and_yield(PasswdEntry.new('www', 800, 800, '/var/www', '/bin/false', 'Serving the web since 1970'))
+      @ohai._require_plugin("passwd")
+      @ohai[:etc][:passwd]['root'].should == Mash.new(:shell => '/bin/zsh', :gecos => 'BOFH', :gid => 1, :uid => 1, :dir => '/root')
+      @ohai[:etc][:passwd]['www'].should == Mash.new(:shell => '/bin/false', :gecos => 'Serving the web since 1970', :gid => 800, :uid => 800, :dir => '/var/www')
+    end
+    
+    it "should set the available groups" do
+      Etc.should_receive(:group).and_yield(GroupEntry.new("admin", 100, ['root', 'chef'])).and_yield(GroupEntry.new('www', 800, ['www', 'deploy']))
+      @ohai._require_plugin("passwd")
+      @ohai[:etc][:group]['admin'].should == Mash.new(:gid => 100, :members => ['root', 'chef'])
+      @ohai[:etc][:group]['www'].should == Mash.new(:gid => 800, :members => ['www', 'deploy'])
+    end
   end
 end

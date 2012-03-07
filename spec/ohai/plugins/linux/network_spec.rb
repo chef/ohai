d1be83ab202df7fff3276f4f63c7ea12fbfb90cf
Bryan McLellan <btm@loftninjas.org>
2012-03-06 18:02:45 -0800

    OHAI-325: add unit tests for subinterfaces

--- spec/ohai/plugins/linux/network_spec.rb
+++ spec/ohai/plugins/linux/network_spec.rb
@@ -39,6 +39,42 @@ eth0      Link encap:Ethernet  HWaddr 12:31:3D:02:BE:A2
           RX bytes:1392844460 (1.2 GiB)  TX bytes:691785313 (659.7 MiB)
           Interrupt:16 
 
+eth0.11   Link encap:Ethernet  HWaddr 00:aa:bb:cc:dd:ee  
+          inet addr:192.168.0.16  Bcast:192.168.0.255  Mask:255.255.255.0
+          inet6 addr: fe80::2aa:bbff:fecc:ddee/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:1208795008 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:3269635153 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:0 
+          RX bytes:1751940374 (1.6 GiB)  TX bytes:2195567597 (2.0 GiB)
+
+eth0.151  Link encap:Ethernet  HWaddr 00:aa:bb:cc:dd:ee  
+          inet addr:10.151.0.16  Bcast:10.151.0.255  Mask:255.255.255.0
+          inet6 addr: fe80::2aa:bbff:fecc:ddee/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:206553677 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:163901336 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:0 
+          RX bytes:3190792261 (2.9 GiB)  TX bytes:755086548 (720.1 MiB)
+
+eth0.152  Link encap:Ethernet  HWaddr 00:aa:bb:cc:dd:ee  
+          inet addr:10.152.1.16  Bcast:10.152.3.255  Mask:255.255.252.0
+          inet6 addr: fe80::2aa:bbff:fecc:ddee/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:14016741 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:55232 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:0 
+          RX bytes:664957462 (634.1 MiB)  TX bytes:4876434 (4.6 MiB)
+
+eth0.153  Link encap:Ethernet  HWaddr 00:aa:bb:cc:dd:ee  
+          inet addr:10.153.1.16  Bcast:10.153.3.255  Mask:255.255.252.0
+          inet6 addr: fe80::2aa:bbff:fecc:ddee/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:2022667595 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:1798627472 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:0 
+          RX bytes:4047036732 (3.7 GiB)  TX bytes:3451231474 (3.2 GiB)
+
 lo        Link encap:Local Loopback  
           inet addr:127.0.0.1  Mask:255.0.0.0
           inet6 addr: ::1/128 Scope:Host
@@ -61,6 +97,27 @@ ENDIFCONFIG
     inet 10.116.201.75/32 scope global eth0
     inet 10.116.201.74/24 scope global secondary eth0
     inet6 fe80::1031:3dff:fe02:bea2/64 scope link 
+       valid_lft forever preferred_lft forever
+3: eth0.11@eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP 
+    link/ether 00:aa:bb:cc:dd:ee brd ff:ff:ff:ff:ff:ff
+    inet 192.168.0.16/24 brd 192.168.0.255 scope global eth0.11
+    inet6 fe80::2e0:81ff:fe2b:48e7/64 scope link 
+       valid_lft forever preferred_lft forever
+4: eth0.151@eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP 
+    link/ether 00:aa:bb:cc:dd:ee brd ff:ff:ff:ff:ff:ff
+    inet 10.151.0.16/24 brd 10.151.0.255 scope global eth0.151
+    inet 10.151.1.16/24 scope global eth0.151
+    inet6 fe80::2e0:81ff:fe2b:48e7/64 scope link 
+       valid_lft forever preferred_lft forever
+5: eth0.152@eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP 
+    link/ether 00:aa:bb:cc:dd:ee brd ff:ff:ff:ff:ff:ff
+    inet 10.152.1.16/22 brd 10.152.3.255 scope global eth0.152
+    inet6 fe80::2e0:81ff:fe2b:48e7/64 scope link 
+       valid_lft forever preferred_lft forever
+6: eth0.153@eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP 
+    link/ether 00:aa:bb:cc:dd:ee brd ff:ff:ff:ff:ff:ff
+    inet 10.153.1.16/22 brd 10.153.3.255 scope global eth0.153
+    inet6 fe80::2e0:81ff:fe2b:48e7/64 scope link 
        valid_lft forever preferred_lft forever
 IP_ADDR
 
@@ -141,7 +198,7 @@ IP_ROUTE
       end
 
       it "detects the interfaces" do
-        @ohai['network']['interfaces'].keys.sort.should == ["eth0", "lo"]
+        @ohai['network']['interfaces'].keys.sort.should == ["eth0", "eth0.11", "eth0.151", "eth0.152", "eth0.153", "lo"]
       end
 
       it "detects the ipv4 addresses of the ethernet interface" do
@@ -149,6 +206,13 @@ IP_ROUTE
         @ohai['network']['interfaces']['eth0']['addresses']['10.116.201.76']['netmask'].should == '255.255.255.0'
         @ohai['network']['interfaces']['eth0']['addresses']['10.116.201.76']['broadcast'].should == '10.116.201.255'
         @ohai['network']['interfaces']['eth0']['addresses']['10.116.201.76']['family'].should == 'inet'
+      end
+
+      it "detects the ipv4 addresses of an ethernet subinterface" do
+        @ohai['network']['interfaces']['eth0.11']['addresses'].keys.should include('192.168.0.16')
+        @ohai['network']['interfaces']['eth0.11']['addresses']['192.168.0.16']['netmask'].should == '255.255.255.0'
+        @ohai['network']['interfaces']['eth0.11']['addresses']['192.168.0.16']['broadcast'].should == '192.168.0.255'
+        @ohai['network']['interfaces']['eth0.11']['addresses']['192.168.0.16']['family'].should == 'inet'
       end
 
       it "detects the ipv6 addresses of the ethernet interface" do
@@ -275,20 +339,56 @@ IP_ROUTE
         @ohai.stub!(:popen4).with("ip neighbor show").and_yield(nil, @stdin_ipneighbor, @ipneighbor_lines, nil)
         @ohai.stub!(:popen4).with("ip addr").and_yield(nil, @stdin_ipaddr, @ipaddr_lines, nil)
         @ohai.stub!(:popen4).with("ip -s link").and_yield(nil, @stdin_iplink, @iplink_lines, nil)
-        @ohai._require_plugin("network")
-        @ohai._require_plugin("linux::network")
       end
 
-      it "finds the default interface by asking which iface has the default route" do
-        @ohai['network'][:default_interface].should == 'eth0'
+      describe "without a subinterface" do
+        before do
+          @ohai._require_plugin("network")
+          @ohai._require_plugin("linux::network")
+        end
+  
+        it "finds the default interface by asking which iface has the default route" do
+          @ohai['network'][:default_interface].should == 'eth0'
+        end
+  
+        it "finds the default interface by asking which iface has the default route" do
+          @ohai['network'][:default_gateway].should == '10.116.201.1'
+        end
       end
+  
+      describe "with a subinterface" do
+        before do
+          linux_ip_route_show_exact = <<-IP_ROUTE
+default via 192.168.0.15 dev eth0.11
+IP_ROUTE
+          linux_route_n = <<-ROUTE_N
+Kernel IP routing table
+Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+192.168.0.0    0.0.0.0         255.255.255.0   U     0      0        0 eth0.11
+10.151.0.0      0.0.0.0         255.255.255.0   U     0      0        0 eth0.151
+10.151.1.0      0.0.0.0         255.255.255.0   U     0      0        0 eth0.151
+10.152.0.0      0.0.0.0         255.255.252.0   U     0      0        0 eth0.152
+10.153.0.0      0.0.0.0         255.255.252.0   U     0      0        0 eth0.153
+0.0.0.0         192.168.0.15   0.0.0.0         UG    0      0        0 eth0.11
+ROUTE_N
 
-      it "finds the default interface by asking which iface has the default route" do
-        @ohai['network'][:default_gateway].should == '10.116.201.1'
+          @iproute_lines = linux_ip_route_show_exact
+          @route_lines = linux_route_n.split("\n")
+          @ohai.stub!(:from).with("route -n \| grep -m 1 ^0.0.0.0").and_return(@route_lines.last)
+          @ohai.stub!(:from).with("ip route show exact 0.0.0.0/0").and_return(@iproute_lines)
+          @ohai._require_plugin("network")
+          @ohai._require_plugin("linux::network")
+        end
+  
+        it "finds the default interface by asking which iface has the default route" do
+          @ohai['network'][:default_interface].should == 'eth0.11'
+        end
+  
+        it "finds the default interface by asking which iface has the default route" do
+          @ohai['network'][:default_gateway].should == '192.168.0.15'
+        end
       end
     end
-
   end
-
 end
 

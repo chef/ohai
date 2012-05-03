98189d018f15acb1f55d7e20177ec9e7d3c1d575
Dmytro Ilchenko <dmytro.ilchenko@livingsocial.com>
2012-05-02 21:12:10 -0400

    Added comment explaining why read_procfile needs to be used for reading /proc/ file

--- lib/ohai/mixin/read_procfile.rb
+++ lib/ohai/mixin/read_procfile.rb
@@ -1,4 +1,6 @@
 class ::File
+  # Use this to read files under /proc directory on linux in a non-blocking way. File.read.each and alike hangs
+  # on linux kernels prior to 2.6.30 (which includes RHEL 5.7 current kernel as of 05/01/2012) due to a kernel bug, making shef unusable.
   def self.read_procfile(path)
     contents = ''
     File.open(path) do |f|

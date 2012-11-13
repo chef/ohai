33727d357c13d0a2a7a04cdf43cf2c5f9f96a22f
Bryan McLellan <btm@loftninjas.org>
2012-11-13 14:53:38 -0500

    Return to former method of expanding keys
    Calling File.expand_path on windows does something very different than on
    unix. While we can possibly [ab]use it for URLs on the latter, this is not
    cross platform.

--- lib/ohai/mixin/ec2_metadata.rb
+++ lib/ohai/mixin/ec2_metadata.rb
@@ -64,7 +64,7 @@ module Ohai
       def fetch_metadata(id='')
         metadata = Hash.new
         http_client.get("#{EC2_METADATA_URL}/#{id}").body.split("\n").each do |o|
-          key = expand_path("#{id}#{o}")
+          key = "#{id}#{o.gsub(/\=.*$/, '/')}"
           if key[-1..-1] != '/'
             metadata[metadata_key(key)] =
               if EC2_ARRAY_VALUES.include? key
@@ -122,13 +122,6 @@ module Ohai
       end
 
       private
-
-      def expand_path(file_name)
-        path = File.expand_path(file_name.gsub(/\=.*$/, '/'), '/')
-        path[0] = '' # remove the initial "/"
-        path << '/' if file_name[-1..-1].eql?('/') # it was a directory
-        path
-      end
 
       def metadata_key(key)
         key.gsub(/\-|\//, '_')

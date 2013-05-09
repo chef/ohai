1706263226499930e6d7b1e390e452fd79c03993
jtimberman <joshua@opscode.com>
2013-05-09 16:00:22 -0600

    [WIP] - converting sigar -> AIX commands

--- lib/ohai/plugins/aix/platform.rb
+++ lib/ohai/plugins/aix/platform.rb
@@ -1,6 +1,6 @@
 #
-# Author:: Doug MacEachern <dougm@vmware.com>
-# Copyright:: Copyright (c) 2010 VMware, Inc.
+# Author:: Joshua Timberman <joshua@opscode.com>
+# Copyright:: Copyright (c) 2013, Opscode, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,4 +16,10 @@
 # limitations under the License.
 #
 
-require_plugin "sigar::platform"
+require_plugin "#{os}::kernel"
+
+provides "platform", "platform_version", "platform_family"
+
+platform kernel[:name]
+platform_version [kernel[:version], kernel[:release]].join(".")
+platform_family platform

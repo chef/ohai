780f1e2d7d21de3bbe3a582b3bdd0987ab8a3c08
Theodore Nordsieck <theo@opscode.com>
2013-07-30 15:53:52 -0700

    added Apache header to files in spec/unit/path

--- spec/unit/path/grab_data.rb
+++ spec/unit/path/grab_data.rb
@@ -1,3 +1,21 @@
+
+# Author:: Theodore Nordsieck <theo@opscode.com>
+# Copyright:: Copyright (c) 2013 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 ##
 ## This is a simple tool to grab shell command output from the local machine.
 ##
@@ -6,7 +24,6 @@
 ##   appropriate yaml file is in this directory, this tool will append/overwrite
 ##   the data in that file as appropriate.
 ##
-
 
 require 'yaml'
 require 'set'
--- spec/unit/path/ohai_plugin_common_spec.rb
+++ spec/unit/path/ohai_plugin_common_spec.rb
@@ -1,3 +1,21 @@
+
+# Author:: Theodore Nordsieck <theo@opscode.com>
+# Copyright:: Copyright (c) 2013 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 require File.expand_path(File.dirname(__FILE__) + "/ohai_plugin_common.rb")
 
 describe OhaiPluginCommon, "subsumes?" do
@@ -25,5 +43,11 @@ describe OhaiPluginCommon, "subsumes?" do
   it "returns true if the second hash contains a key pointing to a nil where the first hash has nothing" do
     @opc.subsumes?( @hash, { "languages" => { "lua" => nil }} ).should be_true
     @opc.subsumes?( @hash, { "languages" => { "python" => { "version" => "1.6.2" }, "lua" => nil }} ).should be_true
+  end
+
+  it "returns false if the second hash has nil in the place of a real value" do
+    @opc.subsumes?( @hash, { "languages" => { "python" => { "version" => nil }}} ).should be_false
+    @opc.subsumes?( @hash, { "languages" => { "python" => nil }} ).should be_false
+    @opc.subsumes?( { "languages" => {}}, { "languages" => nil } ).should be_false
   end
 end

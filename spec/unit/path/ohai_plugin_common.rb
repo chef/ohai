84d3a842879b0a8570f76d2fffa927ecae73a386
Theodore Nordsieck <theo@opscode.com>
2013-08-09 11:02:51 -0700

    disable windows

--- spec/unit/path/ohai_plugin_common.rb
+++ spec/unit/path/ohai_plugin_common.rb
@@ -153,6 +153,8 @@ end
 shared_context "cross platform data" do
   shared_examples_for "a plugin" do |plugin_names, data, cmd_list|
     data.each do |e|
+      next if RUBY_PLATFORM =~ /cygwin|windows|mingw/ #disable for windows
+
       e[:platform].each do |platform|
         e[:arch].each do |arch|
           e[:env].each do |env|

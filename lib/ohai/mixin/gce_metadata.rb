f5f2e14658581080526e99dabc641fb6ae78c65a
Paul Rossman <paul@factory9.com>
2013-08-20 13:41:07 -0500

    updated metadata hostname

--- lib/ohai/mixin/gce_metadata.rb
+++ lib/ohai/mixin/gce_metadata.rb
@@ -23,7 +23,7 @@ module Ohai
 
       extend self
 
-      GCE_METADATA_ADDR = "metadata.google.internal" unless defined?(GCE_METADATA_ADDR)
+      GCE_METADATA_ADDR = "metadata" unless defined?(GCE_METADATA_ADDR)
       GCE_METADATA_URL = "/computeMetadata/v1beta1/?recursive=true" unless defined?(GCE_METADATA_URL)
 
       def can_metadata_connect?(addr, port, timeout=2)

06bcf4e2a4c68c90ad703394d3181c61be4dac2c
Theodore Nordsieck <theo@opscode.com>
2013-08-20 17:13:52 -0700

    formatting

--- spec/unit/plugins/erlang_spec.rb
+++ spec/unit/plugins/erlang_spec.rb
@@ -103,14 +103,14 @@ describe Ohai::System, "plugin erlang" do
 
   tc.call([ "centos-5.9", "centos-6.4", "ubuntu-10.04", "ubuntu-12.04" ], [ "x86", "x64" ], [[]],
           { "languages" => { "erlang" => nil }})
-  tc.call([ "ubuntu-13.04" ],                                             [ "x64" ],        [[]],
+  tc.call([ "ubuntu-13.04" ], [ "x64" ], [[]],
           { "languages" => { "erlang" => nil }})
-  tc.call([ "centos-5.9", "centos-6.4" ],                                 [ "x86", "x64" ], [[ "erlang" ]],
+  tc.call([ "centos-5.9", "centos-6.4" ], [ "x86", "x64" ], [[ "erlang" ]],
           { "languages" => { "erlang" => { "version" => "5.8.5", "options" => [ "ASYNC_THREADS" ], "emulator" => "BEAM" }}})
-  tc.call([ "ubuntu-10.04" ],                                             [ "x86", "x64" ], [[ "erlang" ]],
+  tc.call([ "ubuntu-10.04" ], [ "x86", "x64" ], [[ "erlang" ]],
           { "languages" => { "erlang" => { "version" => "5.7.4", "options" => [ "ASYNC_THREADS", "HIPE" ], "emulator" => "BEAM" }}})
-  tc.call([ "ubuntu-12.04" ],                                             [ "x86", "x64" ], [[ "erlang" ]],
+  tc.call([ "ubuntu-12.04" ], [ "x86", "x64" ], [[ "erlang" ]],
           { "languages" => { "erlang" => { "version" => "5.8.5", "options" => [ "ASYNC_THREADS" ], "emulator" => "BEAM" }}})
-  tc.call([ "ubuntu-13.04" ],                                             [ "x64" ],        [[ "erlang" ]],
+  tc.call([ "ubuntu-13.04" ], [ "x64" ], [[ "erlang" ]],
           { "languages" => { "erlang" => { "version" => "5.9.1", "options" => [ "ASYNC_THREADS" ], "emulator" => "BEAM" }}})
 end

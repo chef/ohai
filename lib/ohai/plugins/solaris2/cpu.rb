b643b78a34b1a89f570348cb861a216b323d3b44
Blake Irvin & Eric Saxby <pair+blake+sax@wanelo.com>
2013-08-27 13:19:08 -0700

    Add cpu check for solaris2

--- lib/ohai/plugins/solaris2/cpu.rb
+++ lib/ohai/plugins/solaris2/cpu.rb
@@ -1,33 +1,41 @@
-#$ psrinfo -v
-#Status of virtual processor 0 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:28.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
-#Status of virtual processor 1 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:30.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
-#Status of virtual processor 2 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:30.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
-#Status of virtual processor 3 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:30.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
-#Status of virtual processor 4 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:30.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
-#Status of virtual processor 5 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:30.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
-#Status of virtual processor 6 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:30.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
-#Status of virtual processor 7 as of: 01/11/2009 23:31:55
-#  on-line since 05/29/2008 15:05:30.
-#  The i386 processor operates at 2660 MHz,
-#        and has an i387 compatible floating point processor.
+#
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+Ohai.plugin do
+  provides "cpu"
+
+  collect_data do
+    cpu Mash.new
+    cpu[:total] = from("psrinfo | wc -l").to_i
+    cpu[:real] = from("psrinfo -p").to_i
+
+    processor_info = from("psrinfo -v -p | grep Hz")
+    processors = processor_info.split(/^    [^\s]/)
+    processors.each_with_index do |processor, i|
+      cpu_info, model_name = processor.split("\n      ")
+      cpu_info = cpu_info.tr("()","").split
+
+      index = i.to_s
+      cpu[index] = Mash.new
+      cpu[index]['vendor_id'] = cpu_info[1]
+      cpu[index]['family'] = cpu_info[4]
+      cpu[index]['model'] = cpu_info[6]
+      cpu[index]['stepping'] = cpu_info[8]
+      cpu[index]['model_name'] = model_name.strip
+      cpu[index]['mhz'] = cpu_info[10]
+    end
+  end
+end

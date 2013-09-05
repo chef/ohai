0a33d81ed7b998c28db819a0228cfe9fe0980004
Robin Ricard <ricard.robin@gmail.com>
2013-09-05 15:15:10 +0200

    Update cpu.rb

--- lib/ohai/plugins/smartos/cpu.rb
+++ lib/ohai/plugins/smartos/cpu.rb
@@ -1,6 +1,10 @@
 # Add cpu to Ohai, it is not implemented for SmartOS in Ohai
-provides "cpu"
+Ohai.plugin do
+  provides "cpu"
 
-cpu Mash.new
-cpu[:total] = `psrinfo | wc -l`.chomp.to_i
-cpu[:real]  = `psrinfo -p`.chomp.to_i
+  collect_data do
+    cpu Mash.new
+    cpu[:total] = `psrinfo | wc -l`.chomp.to_i
+    cpu[:real]  = `psrinfo -p`.chomp.to_i
+  end
+end

ad973176bb22acaaa90b221b167d0cf3b3332c81
Lamont Granquist <lamont@scriptkiddie.org>
2013-10-11 11:43:52 -0700

    swap order of redirects
    - you need to dup2 before redirecting

--- lib/ohai/plugins/java.rb
+++ lib/ohai/plugins/java.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,8 +22,8 @@ require_plugin "languages"
 java = Mash.new
 
 status, stdout, stderr = nil
-if RUBY_PLATFORM.downcase.include?("darwin") 
-  if system("/usr/libexec/java_home 2>&1 >/dev/null")
+if RUBY_PLATFORM.downcase.include?("darwin")
+  if system("/usr/libexec/java_home >/dev/null 2>&1")
     status, stdout, stderr = run_command(:no_status_check => true, :command => "java -version")
   end
 else

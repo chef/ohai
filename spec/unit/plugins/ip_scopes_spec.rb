ae2b585efc8093274c7a5ba0b0710664c4a280b1
Eric Saxby <sax@livinginthepast.org>
2013-11-23 13:50:43 -0800

    OHAI-529 Remove ipaddr_extension from Gemfile
    Specs for the ip_scopes plugin can be run in two ways:
    * Run rspec in the context of `bundle exec`
    * Install ipaddr_extensions, then run rspec outside of
      `bundle exec`

--- spec/unit/plugins/ip_scopes_spec.rb
+++ spec/unit/plugins/ip_scopes_spec.rb
@@ -1,5 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
 
+begin
+  require 'ipaddr_extensions'
+rescue LoadError
+end
+
 describe Ohai::System, "plugin ip_scopes" do
   let(:plugin) { get_plugin('ip_scopes') }
   let(:network) { Mash.new(:interfaces => interfaces) }
@@ -15,72 +20,76 @@ describe Ohai::System, "plugin ip_scopes" do
 
   before { plugin[:network] = network }
 
-  context 'with ipaddr_extensions gem' do
-    let(:ip1) { '10.0.0.1' }
-    let(:ip2) { '1.2.3.4' }
-    let(:ip3) { 'fe80::8638:35ff:fe4e:dc74' }
+  if defined?(IPAddrExtensions)
+    context 'with ipaddr_extensions gem' do
+      let(:ip1) { '10.0.0.1' }
+      let(:ip2) { '1.2.3.4' }
+      let(:ip3) { 'fe80::8638:35ff:fe4e:dc74' }
 
-    let(:addresses1) { Hash[ip1, {}] }
-    let(:addresses2) { Hash[ip2, {}, ip3, {}] }
+      let(:addresses1) { Hash[ip1, {}] }
+      let(:addresses2) { Hash[ip2, {}, ip3, {}] }
 
-    it 'adds ip_scope to each address' do
-      plugin.run
-      expect(plugin[:network][:interfaces][:eth0][:addresses]['10.0.0.1'][:ip_scope]).to eq('RFC1918 PRIVATE')
-      expect(plugin[:network][:interfaces][:eth1][:addresses]['1.2.3.4'][:ip_scope]).to eq('GLOBAL UNICAST')
-      expect(plugin[:network][:interfaces][:eth1][:addresses]['fe80::8638:35ff:fe4e:dc74'][:ip_scope]).to eq('LINK LOCAL UNICAST')
-    end
-
-    describe 'privateaddress' do
-      before { plugin.run }
-
-      context 'host has multiple RFC1918 ethernet addresses' do
-        let(:ip1) { '10.0.0.1' }
-        let(:ip2) { '192.168.1.1' }
-        let(:interface1_type) { 'eth' }
-        let(:interface2_type) { 'eth' }
-
-        it 'picks the last address' do
-          expect(plugin[:privateaddress]).to eq('192.168.1.1')
-        end
+      it 'adds ip_scope to each address' do
+        plugin.run
+        expect(plugin[:network][:interfaces][:eth0][:addresses]['10.0.0.1'][:ip_scope]).to eq('RFC1918 PRIVATE')
+        expect(plugin[:network][:interfaces][:eth1][:addresses]['1.2.3.4'][:ip_scope]).to eq('GLOBAL UNICAST')
+        expect(plugin[:network][:interfaces][:eth1][:addresses]['fe80::8638:35ff:fe4e:dc74'][:ip_scope]).to eq('LINK LOCAL UNICAST')
       end
 
-      context 'host has PPP and ethernet RFC1918 addresses' do
-        let(:ip1) { '10.0.0.1' }
-        let(:ip2) { '192.168.1.1' }
-        let(:interface1_type) { 'eth' }
-        let(:interface2_type) { 'ppp' }
+      describe 'privateaddress' do
+        before { plugin.run }
 
-        it 'prefers the eth address' do
-          expect(plugin[:privateaddress]).to eq('10.0.0.1')
+        context 'host has multiple RFC1918 ethernet addresses' do
+          let(:ip1) { '10.0.0.1' }
+          let(:ip2) { '192.168.1.1' }
+          let(:interface1_type) { 'eth' }
+          let(:interface2_type) { 'eth' }
+
+          it 'picks the last address' do
+            expect(plugin[:privateaddress]).to eq('192.168.1.1')
+          end
         end
-      end
 
-      context 'host only has ppp RFC1918 address' do
-        let(:ip1) { '10.0.0.1' }
-        let(:interface1_type) { 'ppp' }
+        context 'host has PPP and ethernet RFC1918 addresses' do
+          let(:ip1) { '10.0.0.1' }
+          let(:ip2) { '192.168.1.1' }
+          let(:interface1_type) { 'eth' }
+          let(:interface2_type) { 'ppp' }
 
-        it 'uses it' do
-          expect(plugin[:privateaddress]).to eq('10.0.0.1')
+          it 'prefers the eth address' do
+            expect(plugin[:privateaddress]).to eq('10.0.0.1')
+          end
+        end
+
+        context 'host only has ppp RFC1918 address' do
+          let(:ip1) { '10.0.0.1' }
+          let(:interface1_type) { 'ppp' }
+
+          it 'uses it' do
+            expect(plugin[:privateaddress]).to eq('10.0.0.1')
+          end
         end
       end
     end
   end
 
-  context 'without the ipaddr_extensions gem' do
-    let(:addresses1) { Hash['10.0.0.1', {}] }
+  unless defined?(IPAddrExtensions)
+    context 'without the ipaddr_extensions gem' do
+      let(:addresses1) { Hash['10.0.0.1', {}] }
 
-    before do
-      # standin for raising on `require 'ipaddr_extensions'`
-      plugin[:network][:interfaces].stub(:keys).and_raise(LoadError)
-      plugin.run
-    end
+      before do
+        # standin for raising on `require 'ipaddr_extensions'`
+        plugin[:network][:interfaces].stub(:keys).and_raise(LoadError)
+        plugin.run
+      end
 
-    it 'does not add ip_scope to address' do
-      expect(plugin[:network][:interfaces][:eth0][:addresses]['10.0.0.1'][:ip_scope]).to be nil
-    end
+      it 'does not add ip_scope to address' do
+        expect(plugin[:network][:interfaces][:eth0][:addresses]['10.0.0.1'][:ip_scope]).to be nil
+      end
 
-    it 'does not add a privateaddress key' do
-      expect(plugin[:privateaddress]).to be nil
+      it 'does not add a privateaddress key' do
+        expect(plugin[:privateaddress]).to be nil
+      end
     end
   end
 end

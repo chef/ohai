8b40580a6e2218ce74cb222b591ffb21d22ff0d2
Lamont Granquist <lamont@scriptkiddie.org>
2014-01-06 13:12:16 -0800

    catch and ignore failure to load chef
    if chef version is 11.8.2 and it pins ohai to <~ 6.0, then if we
    install ohai 7, the chef plugin will attempt to load chef up and
    then will puke hard since ohai 7 is already loaded.  this just skips
    chef version detection if that dependency resolution problem occurs.

--- lib/ohai/plugins/chef.rb
+++ lib/ohai/plugins/chef.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,11 @@ Ohai.plugin(:Chef) do
   provides "chef_packages/chef"
 
   collect_data do
-    require 'chef/version'
+    begin
+      require 'chef/version'
+    rescue Gem::LoadError
+      next
+    end
 
     chef_packages Mash.new unless chef_packages
     chef_packages[:chef] = Mash.new

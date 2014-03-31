cf6e4ac05dad2d2069cf2f8c8c8343180e66c01b
sersut <serdar@opscode.com>
2014-03-30 21:24:28 -0700

    Version bump for RC release.

--- lib/ohai/version.rb
+++ lib/ohai/version.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,6 +18,5 @@
 
 module Ohai
   OHAI_ROOT = File.expand_path(File.dirname(__FILE__))
-  VERSION = '6.20.0'
+  VERSION = '6.22.0.rc.0'
 end
-

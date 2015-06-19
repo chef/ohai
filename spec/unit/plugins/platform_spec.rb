a876b22a988761637bbafd3f4375a280d61aa7de
danielsdeleo <dan@chef.io>
2015-06-19 11:59:26 -0700

    Fix whitespace in platform spec

--- spec/unit/plugins/platform_spec.rb
+++ spec/unit/plugins/platform_spec.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,20 +32,20 @@ describe Ohai::System, "plugin platform" do
     expect(@plugin[:platform]).to eql("monkey")
     expect(@plugin[:platform_family]).to eql("monkey")
   end
-  
+
   it "should not set the platform to the os if it was set earlier" do
     @plugin[:platform] = 'lars'
     @plugin.run
     expect(@plugin[:platform]).to eql("lars")
   end
-  
+
   it "should set the platform_family to the platform if platform was set earlier but not platform_family" do
     @plugin[:platform] = 'lars'
     @plugin[:platform_family] = 'jack'
     @plugin.run
     expect(@plugin[:platform_family]).to eql("jack")
   end
- 
+
   it "should not set the platform_family if the platform_family was set earlier." do
     @plugin[:platform] = 'lars'
     @plugin.run
@@ -57,7 +57,7 @@ describe Ohai::System, "plugin platform" do
     @plugin.run
     expect(@plugin[:os_version]).to eql("poop")
   end
-  
+
   it "should not set the platform to the os if it was set earlier" do
     @plugin[:platform_version] = 'ulrich'
     @plugin.run

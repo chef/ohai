3ca5eb158288a9d5163e09a312be323a58f7ba76
Shahul Hameed <skhajamohid1@bloomberg.net>
2015-08-13 19:00:29 +0000

    Add support for sparc processors in solaris cpu plugin

--- lib/ohai/plugins/solaris2/cpu.rb
+++ lib/ohai/plugins/solaris2/cpu.rb
@@ -16,12 +16,8 @@
 
 Ohai.plugin(:CPU) do
   provides "cpu"
-
-  collect_data(:solaris2) do
-    cpu Mash.new
-    cpu[:total] = shell_out("psrinfo | wc -l").stdout.to_i
-    cpu[:real] = shell_out("psrinfo -p").stdout.to_i
-
+  
+  def get_x86_processor_info
     processor_info = shell_out("psrinfo -v -p | grep Hz").stdout
     processors = processor_info.split(/^    [^\s]/)
     processors.each_with_index do |processor, i|
@@ -36,6 +32,37 @@ Ohai.plugin(:CPU) do
       cpu[index]['stepping'] = cpu_info[8]
       cpu[index]['model_name'] = model_name.strip
       cpu[index]['mhz'] = cpu_info[10]
+    end
+  end
+
+  def get_sparc_processor_info
+    i = 0
+    cores = 0
+    shell_out('psrinfo -v -p').stdout.lines.each do |line|
+      case  line.strip
+        when /(\d+)\s+cores/
+          cores += "#{$1}".to_i
+        when /^(\S+).*\b(\d+)\s+MHz\)$/
+          index = i.to_s
+          cpu[index] = Mash.new
+          cpu[index]['model_name'] = "#{$1}"
+          cpu[index]['mhz'] = "#{$2}"
+          i += 1
+      end
+    end
+    cpu[:cores] = cores
+  end
+
+  collect_data(:solaris2) do
+    cpu Mash.new
+    cpu[:total] = shell_out("psrinfo | wc -l").stdout.to_i
+    cpu[:real] = shell_out("psrinfo -p").stdout.to_i
+    
+    processor_type = shell_out("uname -p").stdout.strip
+    if processor_type.eql?('sparc')
+        get_sparc_processor_info
+    else
+        get_x86_processor_info
     end
   end
 end

f1778f439c6345ee53b38aaad91b2df971b521bb
Shahul Hameed <skhajamohid1@bloomberg.net>
2015-08-13 19:50:45 +0000

    Add swap space attributes for AIX

--- spec/unit/plugins/aix/memory_spec.rb
+++ spec/unit/plugins/aix/memory_spec.rb
@@ -21,6 +21,8 @@ describe Ohai::System, "AIX memory plugin" do
     @plugin = get_plugin("aix/memory")
     allow(@plugin).to receive(:collect_os).and_return(:aix)
     allow(@plugin).to receive(:shell_out).with("svmon -G -O unit=MB,summary=longreal | grep '[0-9]'").and_return(mock_shell_out(0, " 513280.00 340034.17 173245.83   62535.17 230400.05 276950.14  70176.00\n", nil))
+    @swap_s = "allocated  =    23887872 blocks    used  =   288912 blocks      free  =    23598960 blocks\n"
+    allow(@plugin).to receive(:shell_out).with("swap -s").and_return(mock_shell_out(0,@swap_s, nil))
   end
 
   it "should get total memory" do
@@ -31,5 +33,15 @@ describe Ohai::System, "AIX memory plugin" do
   it "should get free memory" do
     @plugin.run
     expect(@plugin['memory']['free']).to eql("173245.83")
+  end
+  
+  it "should get total swap" do
+    @plugin.run
+    expect(@plugin['memory']['swap']['total']).to eql( (23887872 / 1024.0) * 4)
+  end
+  
+  it "should get free swap" do
+    @plugin.run
+    expect(@plugin['memory']['swap']['free']).to eql( (23598960 / 1024.0) * 4)
   end
 end 

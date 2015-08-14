637bef8bf8c2c6a32ccfdf9186b42fed5de7148b
Shahul Hameed <skhajamohid1@bloomberg.net>
2015-08-14 14:06:57 +0000

    Avoid unnecessary string conversions, use double quotes for consistency everywhere

--- lib/ohai/plugins/solaris2/cpu.rb
+++ lib/ohai/plugins/solaris2/cpu.rb
@@ -26,27 +26,27 @@ Ohai.plugin(:CPU) do
 
       index = i.to_s
       cpu[index] = Mash.new
-      cpu[index]['vendor_id'] = cpu_info[1]
-      cpu[index]['family'] = cpu_info[4]
-      cpu[index]['model'] = cpu_info[6]
-      cpu[index]['stepping'] = cpu_info[8]
-      cpu[index]['model_name'] = model_name.strip
-      cpu[index]['mhz'] = cpu_info[10]
+      cpu[index]["vendor_id"] = cpu_info[1]
+      cpu[index]["family"] = cpu_info[4]
+      cpu[index]["model"] = cpu_info[6]
+      cpu[index]["stepping"] = cpu_info[8]
+      cpu[index]["model_name"] = model_name.strip
+      cpu[index]["mhz"] = cpu_info[10]
     end
   end
 
   def get_sparc_processor_info
     i = 0
     cores = 0
-    shell_out('psrinfo -v -p').stdout.lines.each do |line|
+    shell_out("psrinfo -v -p").stdout.lines.each do |line|
       case  line.strip
         when /(\d+)\s+cores/
-          cores += "#{$1}".to_i
+          cores += $1.to_i
         when /^(\S+).*\b(\d+)\s+MHz\)$/
           index = i.to_s
           cpu[index] = Mash.new
-          cpu[index]['model_name'] = "#{$1}"
-          cpu[index]['mhz'] = "#{$2}"
+          cpu[index]["model_name"] = $1
+          cpu[index]["mhz"] = $2
           i += 1
       end
     end
@@ -59,7 +59,7 @@ Ohai.plugin(:CPU) do
     cpu[:real] = shell_out("psrinfo -p").stdout.to_i
     
     processor_type = shell_out("uname -p").stdout.strip
-    if processor_type.eql?('sparc')
+    if processor_type == "sparc"
         get_sparc_processor_info
     else
         get_x86_processor_info

682e9745aaaf6421bb5cb9d398471de9c3f3ccd7
Tim Smith <tsmith84@gmail.com>
2015-11-29 15:17:28 -0800

    Only poll vmware attributes when on vmware
    This prevents an empty vmware hash with nil values being created on
    azure where the vmware tools are on the ubuntu image.

--- spec/unit/plugins/vmware_spec.rb
+++ spec/unit/plugins/vmware_spec.rb
@@ -16,47 +16,63 @@
 # limitations under the License.
 require 'spec_helper'
 
-describe Ohai::System, "plugin vmware" do
+describe Ohai::System, 'plugin vmware' do
+  let(:plugin) { get_plugin('vmware') }
+  let(:path) { '/usr/bin/vmware-toolbox-cmd' }
 
-  let(:plugin) { get_plugin("vmware") }
-  let(:path) { "/usr/bin/vmware-toolbox-cmd" }
+  before(:each) do
+    allow(plugin).to receive(:collect_os).and_return(:linux)
+  end
 
-  context "vmware toolbox" do
-
-    def setup_stubs
-      allow(File).to receive(:exist?).and_return(true)
-      allow(plugin).to receive(:collect_os).and_return(:linux)
-      allow(plugin).to receive(:shell_out).with("#{path} stat speed").and_return(mock_shell_out(0, "2000 MHz", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat hosttime").and_return(mock_shell_out(0, "04 Jun 2015 19:21:16", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat sessionid").and_return(mock_shell_out(0, "0x0000000000000000", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat balloon").and_return(mock_shell_out(0, "0 MB", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat swap").and_return(mock_shell_out(0, "0 MB", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat memlimit").and_return(mock_shell_out(0, "4000000000 MB", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat memres").and_return(mock_shell_out(0, "0 MB", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat cpures").and_return(mock_shell_out(0, "0 MHz", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} stat cpulimit").and_return(mock_shell_out(0, "4000000000 MB", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} upgrade status").and_return(mock_shell_out(0, "VMware Tools are up-to-date.", nil))
-      allow(plugin).to receive(:shell_out).with("#{path} timesync status").and_return(mock_shell_out(0, "Disabled", nil))
+  context 'on vmware guest with toolbox installed' do
+    before(:each) do
+      allow(File).to receive(:exist?).with('/usr/bin/vmware-toolbox-cmd').and_return(true)
+      allow(plugin).to receive(:shell_out).with("#{path} stat speed").and_return(mock_shell_out(0, '2000 MHz', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat hosttime").and_return(mock_shell_out(0, '04 Jun 2015 19:21:16', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat sessionid").and_return(mock_shell_out(0, '0x0000000000000000', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat balloon").and_return(mock_shell_out(0, '0 MB', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat swap").and_return(mock_shell_out(0, '0 MB', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat memlimit").and_return(mock_shell_out(0, '4000000000 MB', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat memres").and_return(mock_shell_out(0, '0 MB', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat cpures").and_return(mock_shell_out(0, '0 MHz', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} stat cpulimit").and_return(mock_shell_out(0, '4000000000 MB', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} upgrade status").and_return(mock_shell_out(0, 'VMware Tools are up-to-date.', nil))
+      allow(plugin).to receive(:shell_out).with("#{path} timesync status").and_return(mock_shell_out(0, 'Disabled', nil))
+      plugin[:virtualization] = Mash.new
+      plugin[:virtualization][:systems] = Mash.new
+      plugin[:virtualization][:systems][:vmware] = Mash.new
       plugin.run
     end
 
-    before(:each) do
-      setup_stubs
+    it 'gets the speed' do
+      expect(plugin[:vmware][:speed]).to eq('2000 MHz')
     end
 
-    context "the vmware toolbox cmd" do
+    it 'gets the hosttime' do
+      expect(plugin[:vmware][:hosttime]).to eq('04 Jun 2015 19:21:16')
+    end
 
-      it "gets the speed" do
-        expect(plugin[:vmware][:speed]).to eq("2000 MHz")
-      end
+    it 'gets tools update status' do
+      expect(plugin[:vmware][:upgrade]).to eq('VMware Tools are up-to-date.')
+    end
+  end
 
-      it "gets the hosttime" do
-        expect(plugin[:vmware][:hosttime]).to eq("04 Jun 2015 19:21:16")
-      end
+  context 'on vmware guest without toolbox' do
+    it 'should not create a vmware attribute' do
+      plugin[:virtualization] = Mash.new
+      plugin[:virtualization][:systems] = Mash.new
+      plugin[:virtualization][:systems][:vmware] = Mash.new
+      allow(File).to receive(:exist?).with('/usr/bin/vmware-toolbox-cmd').and_return(false)
+      expect(plugin).not_to have_key(:vmware)
+    end
+  end
 
-      it "gets tools update status" do
-        expect(plugin[:vmware][:upgrade]).to eq("VMware Tools are up-to-date.")
-      end
+  context 'on vbox guest' do
+    it 'should not create a vmware attribute' do
+      plugin[:virtualization] = Mash.new
+      plugin[:virtualization][:systems] = Mash.new
+      plugin[:virtualization][:systems][:vbox] = Mash.new
+      expect(plugin).not_to have_key(:vmware)
     end
   end
 end

05e6dfe056896f07fe2dd4f941e571e36bbef9d8
danielsdeleo <dan@chef.io>
2016-02-02 13:07:44 -0800

    Make pnp plugin opt-in via config

--- lib/ohai/plugins/windows/drivers.rb
+++ lib/ohai/plugins/windows/drivers.rb
@@ -22,21 +22,23 @@ Ohai.plugin(:Drivers) do
   collect_data(:windows) do
     require 'wmi-lite/wmi'
 
-    wmi = WmiLite::Wmi.new
-
     kext = Mash.new
     pnp_drivers = Mash.new
 
-    drivers = wmi.instances_of('Win32_PnPSignedDriver')
-    drivers.each do |driver|
-      pnp_drivers[driver['deviceid']] = Mash.new
-      driver.wmi_ole_object.properties_.each do |p|
-        pnp_drivers[driver['deviceid']][p.name.wmi_underscore.to_sym] = driver[p.name.downcase]
-      end
-      if driver['devicename']
-        kext[driver['devicename']] = pnp_drivers[driver['deviceid']]
-        kext[driver['devicename']][:version] = pnp_drivers[driver['deviceid']][:driver_version]
-        kext[driver['devicename']][:date] = pnp_drivers[driver['deviceid']][:driver_date] ? pnp_drivers[driver['deviceid']][:driver_date].to_s[0..7] : nil
+    if configuration(:enabled)
+      wmi = WmiLite::Wmi.new
+
+      drivers = wmi.instances_of('Win32_PnPSignedDriver')
+      drivers.each do |driver|
+        pnp_drivers[driver['deviceid']] = Mash.new
+        driver.wmi_ole_object.properties_.each do |p|
+          pnp_drivers[driver['deviceid']][p.name.wmi_underscore.to_sym] = driver[p.name.downcase]
+        end
+        if driver['devicename']
+          kext[driver['devicename']] = pnp_drivers[driver['deviceid']]
+          kext[driver['devicename']][:version] = pnp_drivers[driver['deviceid']][:driver_version]
+          kext[driver['devicename']][:date] = pnp_drivers[driver['deviceid']][:driver_date] ? pnp_drivers[driver['deviceid']][:driver_date].to_s[0..7] : nil
+        end
       end
     end
 

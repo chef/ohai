7f7404a509378fcda224bff7316c85babac89e60
Satish Puranam <satish.puranam@gmail.com>
2016-02-21 12:53:30 -0500

    Fix build, cause by previous commit.

--- lib/ohai/plugins/linux/platform.rb
+++ lib/ohai/plugins/linux/platform.rb
@@ -124,8 +124,8 @@ Ohai.plugin(:Platform) do
       suse_version = suse_release.scan(/VERSION = (\d+)\nPATCHLEVEL = (\d+)/).flatten.join(".")
       suse_version = suse_release[/VERSION = ([\d\.]{2,})/, 1] if suse_version == ""
       platform_version suse_version
-      if suse_release =~ /^openSUSE/
-        platform "opensuse" && platform_version.to_i < 42
+      if suse_release =~ /^openSUSE/ && platform_version.to_i < 42
+        platform "opensuse"
       else
         platform "suse"
       end

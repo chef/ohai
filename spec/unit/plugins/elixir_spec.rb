63cbc4649ded4ed7b0e122ac8aa861a143c34b28
Tim Smith <tsmith84@gmail.com>
2016-04-15 13:58:17 -0700

    Fix Elixir version detection on newer Elixir releases
    Add specs for the new format + error logging + general fixes

--- spec/unit/plugins/elixir_spec.rb
+++ spec/unit/plugins/elixir_spec.rb
@@ -17,29 +17,39 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "/spec_helper.rb"))
 
 describe Ohai::System, "plugin elixir" do
+  let(:plugin) { get_plugin("elixir") }
 
   before(:each) do
-    @plugin = get_plugin("elixir")
-    @plugin[:languages] = Mash.new
-    @stdout = "Elixir 1.0.2"
-    allow(@plugin).to receive(:shell_out).with("elixir -v").and_return(mock_shell_out(0, @stdout, ""))
+    plugin[:languages] = Mash.new
   end
 
-  it "should get the elixir version" do
-    expect(@plugin).to receive(:shell_out).with("elixir -v").and_return(mock_shell_out(0, @stdout, ""))
-    @plugin.run
+  it "should shellout to elixir -v" do
+    expect(plugin).to receive(:shell_out).with("elixir -v").and_return(mock_shell_out(0, "Elixir 1.0.2", ""))
+    plugin.run
   end
 
-  it "should set languages[:elixir][:version]" do
-    @plugin.run
-    expect(@plugin.languages[:elixir][:version]).to eql("1.0.2")
+  it "sets languages[:elixir][:version] on older elixir" do
+    allow(plugin).to receive(:shell_out).with("elixir -v").and_return(mock_shell_out(0, "Elixir 1.0.2", ""))
+    plugin.run
+    expect(plugin.languages[:elixir][:version]).to eql("1.0.2")
   end
 
-  it "should not set the languages[:elixir] if elixir command fails" do
-    @stdout = "Elixir 1.0.2\n"
-    allow(@plugin).to receive(:shell_out).with("elixir -v").and_return(mock_shell_out(1, @stdout, ""))
-    @plugin.run
-    expect(@plugin.languages).not_to have_key(:elixir)
+  it "sets languages[:elixir][:version] on newer elixir" do
+    new_stdout = "Erlang/OTP 18 [erts-7.3] [source] [64-bit] [smp:8:8] [async-threads:10] [hipe] [kernel-poll:false] [dtrace]\n\nElixir 1.2.4\n"
+    allow(plugin).to receive(:shell_out).with("elixir -v").and_return(mock_shell_out(0, new_stdout, ""))
+    plugin.run
+    expect(plugin.languages[:elixir][:version]).to eql("1.2.4")
   end
 
+  it "does not set languages[:elixir] if elixir command fails" do
+    allow(plugin).to receive(:shell_out).with("elixir -v").and_return(mock_shell_out(1, "", ""))
+    plugin.run
+    expect(plugin.languages).not_to have_key(:elixir)
+  end
+
+  it "does not set languages[:elixir] if elixir command doesn't exist" do
+    allow(plugin).to receive(:shell_out).and_raise(Ohai::Exceptions::Exec)
+    plugin.run
+    expect(plugin.languages).not_to have_key(:elixir)
+  end
 end

78f8c06100f1b121b0139d7209680548bb88fdbd
Steven Murawski <steven.murawski@gmail.com>
2016-06-21 12:37:32 -0500

    remove require base64

--- lib/ohai/plugins/powershell.rb
+++ lib/ohai/plugins/powershell.rb
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-require "base64"
 
 Ohai.plugin(:Powershell) do
   provides "languages/powershell"

7dd82d25e70b1d58076a25eccec97bf272bb02b3
Jose Asuncion <jeunito@gmail.com>
2016-09-01 00:43:03 -0700

    detect lxd

--- lib/ohai/plugins/linux/virtualization.rb
+++ lib/ohai/plugins/linux/virtualization.rb
@@ -194,5 +194,15 @@ Ohai.plugin(:Virtualization) do
       virtualization[:role] = "guest"
       virtualization[:systems][:docker] = "guest"
     end
+
+    # Detect LXD
+    # See https://github.com/lxc/lxd/blob/master/doc/dev-lxd.md
+    if File.exist?("/dev/lxd/sock")
+      virtualization[:system] = "lxd"
+      virtualization[:role] = "guest"
+    elsif File.exist?("/var/lib/lxd/devlxd")
+      virtualization[:system] = "lxd"
+      virtualization[:role] = "host"
+    end
   end
 end

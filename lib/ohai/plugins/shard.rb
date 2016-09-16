528777e63f279b4591f04b20b7687099849af5e3
Phil Dibowitz <phil@ipom.com>
2016-09-15 17:49:06 -0700

    Add shard plugin
    This adds a shard_seed that people can then shard on ala:
    
    ```
    def in_shard(node, shard, shard_size)
      node['shard_seed'] % shard_size <= shard
    end
    ```
    
    Which then enables:
    
    ```
    if in_shard(node, 4, 100)
      # some new stuff
    else
      # some old stuff
    end
    ```
    
    Which is roughly how we shard things. Other code to utilize this to follow.

---
+++ lib/ohai/plugins/shard.rb
@@ -0,0 +1,43 @@
+#
+# Author:: Phil Dibowitz <phil@ipom.com>
+# Copyright:: Copyright (c) 2016 Facebook, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+def get_dmi_thing(dmi, thing)
+  %w{system base_board chassis}.each do |section|
+    unless dmi[:thing][thing].compact.empty?
+      return dmi[:thing][thing]
+    end
+  end
+end
+
+Ohai.plugin(:ShardSeed) do
+  depends "hostname", "dmi"
+  provides "shard_seed"
+
+  collect_data(:default) do
+    base = Ohai.config[:plugin][:shard_seed][:base] || :hostname
+    data = case base
+           when :hostname
+             fqdn
+           when :serial
+             get_dmi_thing(dmi, :serial_number)
+           when :uuid
+             get_dmi_thing(dmi, :uuid)
+           end
+    shard_seed Digest::MD5.hexdigest(data)[0...7].to_i(16)
+  end
+end

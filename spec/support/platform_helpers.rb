c8282b416b330decefe56caca7428fc7f38d6144
Tim Smith <tsmith@chef.io>
2016-11-09 19:26:30 -0800

    Remove Ruby 1.8 / 1.9 spec helpers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/support/platform_helpers.rb
+++ spec/support/platform_helpers.rb
@@ -1,16 +1,6 @@
-def ruby_19?
-  !!(RUBY_VERSION =~ /^1.9/)
-end
-
-def ruby_18?
-  !!(RUBY_VERSION =~ /^1.8/)
-end
-
 def windows?
   !!(RUBY_PLATFORM =~ /mswin|mingw|windows/)
 end
-
-# def jruby?
 
 def unix?
   !windows?

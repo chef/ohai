a927aa923604d03e98aa32fffdb5caee7424968c
Tim Smith <tsmith@chef.io>
2017-01-20 13:54:33 -0800

    Update GCC sample output

--- lib/ohai/plugins/c.rb
+++ lib/ohai/plugins/c.rb
@@ -48,21 +48,23 @@ Ohai.plugin(:C) do
   end
 
   def collect_gcc
-    #gcc
-    collect("gcc -v") do |so|
-      # Sample output:
-      # Configured with: --prefix=/Applications/Xcode.app/Contents/Developer/usr --with-gxx-include-dir=/usr/include/c++/4.2.1
-      # Apple LLVM version 7.3.0 (clang-703.0.29)
-      # Target: x86_64-apple-darwin15.4.0
-      # Thread model: posix
-      # InstalledDir: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
-      description = so.stderr.split($/).last
-      output = description.split
-      if output.length >= 3
-        c[:gcc] = Mash.new
-        c[:gcc][:version] = output[2]
-        c[:gcc][:description] = description
-      end
+    # gcc
+    # Sample output on os x:
+    # Configured with: --prefix=/Applications/Xcode.app/Contents/Developer/usr --with-gxx-include-dir=/usr/include/c++/4.2.1
+    # Apple LLVM version 7.3.0 (clang-703.0.29)
+    # Target: x86_64-apple-darwin15.4.0
+    # Thread model: posix
+    # InstalledDir: /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
+    #
+    #
+    # Sample output on Linux:
+    # Using built-in specs.
+    # COLLECT_GCC=gcc
+    # COLLECT_LTO_WRAPPER=/usr/lib/gcc/x86_64-linux-gnu/5/lto-wrapper
+    # Target: x86_64-linux-gnu
+    # Configured with: ../src/configure -v --with-pkgversion='Ubuntu 5.4.0-6ubuntu1~16.04.4' --with-bugurl=file:///usr/share/doc/gcc-5/README.Bugs --enable-languages=c,ada,c++,java,go,d,fortran,objc,obj-c++ --prefix=/usr --program-suffix=-5 --enable-shared --enable-linker-build-id --libexecdir=/usr/lib --without-included-gettext --enable-threads=posix --libdir=/usr/lib --enable-nls --with-sysroot=/ --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --with-default-libstdcxx-abi=new --enable-gnu-unique-object --disable-vtable-verify --enable-libmpx --enable-plugin --with-system-zlib --disable-browser-plugin --enable-java-awt=gtk --enable-gtk-cairo --with-java-home=/usr/lib/jvm/java-1.5.0-gcj-5-amd64/jre --enable-java-home --with-jvm-root-dir=/usr/lib/jvm/java-1.5.0-gcj-5-amd64 --with-jvm-jar-dir=/usr/lib/jvm-exports/java-1.5.0-gcj-5-amd64 --with-arch-directory=amd64 --with-ecj-jar=/usr/share/java/eclipse-ecj.jar --enable-objc-gc --enable-multiarch --disable-werror --with-arch-32=i686 --with-abi=m64 --with-multilib-list=m32,m64,mx32 --enable-multilib --with-tune=generic --enable-checking=release --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu
+    # Thread model: posix
+    # gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.4)
     end
   end
 

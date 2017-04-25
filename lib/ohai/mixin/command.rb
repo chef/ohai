7c969e1e80c2b86eb0583aebb4098e6a693ffba0
Akira Kitada <akitada@gmail.com>
2017-04-26 03:10:28 +0900

    Inject sane paths into shell_out
    To ensure shell_out can find binaries.
    
    Signed-off-by: Akira Kitada <akitada@gmail.com>

--- lib/ohai/mixin/command.rb
+++ lib/ohai/mixin/command.rb
@@ -27,8 +27,13 @@ module Ohai
       # DISCLAIMER: Logging only works in the context of a plugin!!
       # accept a command and any of the mixlib-shellout options
       def shell_out(cmd, **options)
+        options = options.dup
         # unless specified by the caller timeout after 30 seconds
         options[:timeout] ||= 30
+        unless RUBY_PLATFORM =~ /mswin|mingw32|windows/
+          options[:env] = options.key?(:env) ? options[:env].dup : {}
+          options[:env]["PATH"] ||= ((ENV["PATH"] || "").split(":") + %w{/usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin}).join(":")
+        end
         so = Mixlib::ShellOut.new(cmd, options)
         begin
           so.run_command

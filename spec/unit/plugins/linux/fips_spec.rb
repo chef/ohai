816808092a73bae10743e5dedb588ced1c5ea4b7
Tim Smith <tsmith@chef.io>
2018-11-24 14:17:42 -0800

    Remove the Ruby < 2.5 logic from the FIPS plugins
    So much to delete here.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/plugins/linux/fips_spec.rb
+++ spec/unit/plugins/linux/fips_spec.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Matt Wrock (<matt@mattwrock.com>)
-# Copyright:: Copyright (c) 2016 Chef Software, Inc.
+# Copyright:: Copyright (c) 2016-2018 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,8 +22,7 @@ require "openssl"
 describe Ohai::System, "plugin fips" do
   let(:enabled) { "0" }
   let(:plugin) { get_plugin("linux/fips") }
-  let(:fips_path) { "/proc/sys/crypto/fips_enabled" }
-  let(:openssl_test_mode) { true }
+  let(:openssl_test_mode) { false }
 
   subject do
     plugin.run
@@ -32,7 +31,6 @@ describe Ohai::System, "plugin fips" do
 
   before(:each) do
     allow(plugin).to receive(:collect_os).and_return(:linux)
-    allow(::File).to receive(:read).with(fips_path).and_return(enabled)
   end
 
   around do |ex|
@@ -44,47 +42,17 @@ describe Ohai::System, "plugin fips" do
 
   end
 
-  context "fips file is present and contains 1" do
-    let(:enabled) { "1" }
+  context "with OpenSSL.fips_mode == false" do
+    before { allow(OpenSSL).to receive(:fips_mode).and_return(false) }
+    it "does not set fips plugin" do
+      expect(subject).to be(false)
+    end
+  end
 
+  context "with OpenSSL.fips_mode == true" do
+    before { allow(OpenSSL).to receive(:fips_mode).and_return(true) }
     it "sets fips plugin" do
       expect(subject).to be(true)
-    end
-  end
-
-  context "fips file does not contain 1" do
-    let(:enabled) { "0" }
-
-    it "does not set fips plugin" do
-      expect(subject).to be(false)
-    end
-  end
-
-  context "fips file is not present" do
-    before do
-      allow(::File).to receive(:read).and_raise(Errno::ENOENT, "bibbleboop")
-    end
-
-    it "does not set fips plugin" do
-      expect(subject).to be(false)
-    end
-  end
-
-  context "with Ruby 2.5 or newer", if: defined?(OpenSSL.fips_mode) do
-    let(:openssl_test_mode) { false }
-
-    context "with OpenSSL.fips_mode == false" do
-      before { allow(OpenSSL).to receive(:fips_mode).and_return(false) }
-      it "does not set fips plugin" do
-        expect(subject).to be(false)
-      end
-    end
-
-    context "with OpenSSL.fips_mode == true" do
-      before { allow(OpenSSL).to receive(:fips_mode).and_return(true) }
-      it "sets fips plugin" do
-        expect(subject).to be(true)
-      end
     end
   end
 end

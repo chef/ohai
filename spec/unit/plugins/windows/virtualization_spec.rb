3fe73d5eaf251ec1140949beab4e3e661c7eb287
Tim Smith <tsmith@chef.io>
2018-12-02 10:39:54 -0800

    Fix the Windows specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/plugins/windows/virtualization_spec.rb
+++ spec/unit/plugins/windows/virtualization_spec.rb
@@ -22,21 +22,23 @@ require_relative "../../../spec_helper.rb"
 
 describe Ohai::System, "Windows virtualization platform" do
   let(:plugin) { get_plugin("windows/virtualization") }
+  let(:wmi) { double("WmiLite::Wmi") }
 
   before(:each) do
+    allow(WmiLite::Wmi).to receive(:new).and_return(wmi)
     allow(plugin).to receive(:collect_os).and_return(:windows)
   end
 
   describe "it sets virtualization guest status from Win32_ComputerSystemProduct data" do
     it "system is vmware" do
-      allow_any_instance_of(WmiLite::Wmi).to receive(:instances_of).with("Win32_ComputerSystemProduct").and_return( { "caption" => "Computer System Product",
-                                                                                                                      "description" => "Computer System Product",
-                                                                                                                      "identifyingnumber" => "ec2d6aad-f59b-a10d-5784-ca9b7ba4f727",
-                                                                                                                      "name" => "HVM domU",
-                                                                                                                      "skunumber" => nil,
-                                                                                                                      "uuid" => "EC2D6AAD-F59B-A10D-5784-CA9B7BA4F727",
-                                                                                                                      "vendor" => "Xen",
-                                                                                                                      "version" => "4.2.amazon" } )
+      allow(wmi).to receive(:first_of).with("Win32_ComputerSystemProduct").and_return( { "caption" => "Computer System Product",
+                                                                                         "description" => "Computer System Product",
+                                                                                         "identifyingnumber" => "ec2d6aad-f59b-a10d-5784-ca9b7ba4f727",
+                                                                                         "name" => "HVM domU",
+                                                                                         "skunumber" => nil,
+                                                                                         "uuid" => "EC2D6AAD-F59B-A10D-5784-CA9B7BA4F727",
+                                                                                         "vendor" => "Xen",
+                                                                                         "version" => "4.2.amazon" } )
       plugin.run
       expect(plugin[:virtualization][:system]).to eq("xen")
       expect(plugin[:virtualization][:role]).to eq("guest")
@@ -46,14 +48,14 @@ describe Ohai::System, "Windows virtualization platform" do
 
   context "when running on a hardware system" do
     it "does not set virtualization attributes" do
-      allow_any_instance_of(WmiLite::Wmi).to receive(:instances_of).with("Win32_ComputerSystemProduct").and_return({ "caption" => "Computer System Product",
-                                                                                                                     "description" => "Computer System Product",
-                                                                                                                     "identifyingnumber" => "0123456789",
-                                                                                                                     "name" => "X10SLH-N6-ST031",
-                                                                                                                     "skunumber" => nil,
-                                                                                                                     "uuid" => "00000000-0000-0000-0000-0CC47A8F7618",
-                                                                                                                     "vendor" => "Supermicro",
-                                                                                                                     "version" => "0123456789" })
+      allow(wmi).to receive(:first_of).with("Win32_ComputerSystemProduct").and_return({ "caption" => "Computer System Product",
+                                                                                        "description" => "Computer System Product",
+                                                                                        "identifyingnumber" => "0123456789",
+                                                                                        "name" => "X10SLH-N6-ST031",
+                                                                                        "skunumber" => nil,
+                                                                                        "uuid" => "00000000-0000-0000-0000-0CC47A8F7618",
+                                                                                        "vendor" => "Supermicro",
+                                                                                        "version" => "0123456789" })
       plugin.run
       expect(plugin[:virtualization]).to eq("systems" => {})
     end

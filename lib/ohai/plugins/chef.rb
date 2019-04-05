bf40e0797941eb4a03ba61875154da7ddc59c96c
Lynn Frank <franklin.webber@gmail.com>
2019-04-05 15:08:21 -0500

    Adds WIP implementation of finding the chef version
    Signed-off-by: Franklin Webber <franklin.webber@gmail.com>

--- lib/ohai/plugins/chef.rb
+++ lib/ohai/plugins/chef.rb
@@ -19,7 +19,7 @@
 Ohai.plugin(:Chef) do
   provides "chef_packages/chef"
 
-  collect_data do
+  collect_data(:linux) do
     # welcome to a quick implementation that gets the job done.
 
     # Try and find chef. If it results in returning us chef its not on the path
@@ -45,6 +45,45 @@ Ohai.plugin(:Chef) do
       # just put them all into the value here in an array.
       chef_root = shell_out("find /opt -path '*/chef-#{version}/lib'").stdout.chomp.lines.first
       chef_packages[:chef][:chefroot] = chef_root
+    end
+  end
+
+  collect_data(:windows) do
+    # welcome to a quick implementation that gets the job done.
+    chef_client_bin = nil
+    # Try and find chef. If it results in returning us chef its not on the path
+    # So we want to look for it in the usual install locations.
+    chef_client_results = shell_out('get-command chef-client | Select-Object -ExpandProperty Definition')
+    if chef_client_results.exit_status == 0
+      
+      chef_client_bin = chef_client_results.stdout.strip
+    else
+      # TODO: when not found with get-command
+    end
+
+
+    if chef_client_bin
+      chef_app_name, version = shell_out("#{chef_client_bin} --version").stdout.chomp.split(' ')
+
+      chef_packages Mash.new unless chef_packages
+      chef_packages[:chef] = Mash.new
+      chef_packages[:chef][:version] = version
+
+      
+      # There is an assumption that it exists somewhere within this directory.
+      # There could multiple and I'm not sure which one would be prefered. I honestly would
+      # just put them all into the value here in an array.
+      # chef_root = shell_out("find /opt -path '*/chef-#{version}/lib'").stdout.chomp.lines.first
+      # TODO: the C:\\opscode is assumed here that value should be extracted from the
+      chef_base_install_path = chef_client_bin.split("\\")[0..2].join("\\")
+      chef_root_results = shell_out("get-childitem -Path #{chef_base_install_path} -Filter chef-#{version}-universal-mingw32 -Directory -Recurse | Select-Object -ExpandProperty FullName")
+
+      if chef_root_results.exit_status == 0
+        # There are two entries one in extensions and one in gems
+        chef_root = chef_root_results.stdout.strip.split("\r\n")[1]
+        chef_packages[:chef][:chefroot] = chef_root
+      end
+      
     end
   end
 end

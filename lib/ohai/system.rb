a467d06706cfed5e6512e0ac63fe27d92be270a9
Tim Smith <tsmith@chef.io>
2019-05-09 16:59:55 -0700

    Require ohai/version where we use it
    This was missed until automate required just ohai system
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/ohai/system.rb
+++ lib/ohai/system.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Adam Jacob (<adam@chef.io>)
-# Copyright:: Copyright (c) 2008-2017, Chef Software Inc.
+# Copyright:: Copyright (c) 2008-2019, Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,6 +16,7 @@
 # limitations under the License.
 #
 
+require "ohai/version"
 require "ohai/loader"
 require "ohai/log"
 require "ohai/mash"

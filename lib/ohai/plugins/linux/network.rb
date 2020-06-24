d83afc7244cf63be9f27cb424ba754245c2ecbcf
Cooper Lees <me@cooperlees.com>
2020-06-24 11:53:28 -0700

    Linux network plugin: Handle IPv6 next hops for IPv4 routes - Add handling for `via` where the inet6 address family is specified - Add rspec test to show working
    Addresses part of #1474
    
    Signed-off-by: Cooper Lees <me@cooperlees.com>

--- lib/ohai/plugins/linux/network.rb
+++ lib/ohai/plugins/linux/network.rb
@@ -105,6 +105,10 @@ Ohai.plugin(:Network) do
         route_entry = Mash.new(destination: route_dest,
                                family: family[:name])
         %w{via scope metric proto src}.each do |k|
+          if k == 'via' && route_ending =~ /\bvia\sinet6\s+([^\s]+)/
+            route_entry[k] = $1
+            next
+          end
           # http://rubular.com/r/pwTNp65VFf
           route_entry[k] = $1 if route_ending =~ /\b#{k}\s+([^\s]+)/
         end

f11b854cd189e8ad311592d507a0849cfbc0da52
Lamont Granquist <lamont@scriptkiddie.org>
2020-08-21 16:30:54 -0700

    Migrate to the chef-utils helpers
    This replaces the which/shell_out helpers with ones from chef-utils.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/ohai/util/file_helper.rb
+++ lib/ohai/util/file_helper.rb
@@ -1,38 +1,4 @@
-# Author:: Lamont Granquist (<lamont@chef.io>)
-#
-# Copyright:: Copyright (c) 2013-14 Chef Software, Inc.
-#
-# License:: Apache License, Version 2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Copied from chef/lib/chef/util/selinux.rb
-
-module Ohai
-  module Util
-    module FileHelper
-      def which(cmd)
-        paths = ENV["PATH"].split(File::PATH_SEPARATOR) + [ "/bin", "/usr/bin", "/sbin", "/usr/sbin" ]
-        paths.each do |path|
-          filename = File.join(path, cmd)
-          if File.executable?(filename)
-            logger.trace("Plugin #{name}: found #{cmd} at #{filename}")
-            return filename
-          end
-        end
-        logger.trace("Plugin #{name}: did not find #{cmd}")
-        false
-      end
-    end
-  end
+require_relative "../mixin/which"
+module Ohai::Util
+  FileHelper = Ohai::Mixin::Which
 end

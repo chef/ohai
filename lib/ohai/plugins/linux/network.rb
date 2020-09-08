08347833c30a20799c3a4b7240d8e712f23c2ca3
Tim Smith <tsmith84@gmail.com>
2020-09-08 11:51:33 -0700

    Bail out early to avoid safe operator
    Also add some notes here that we should convert this to a filter_map
    when we can as that's easier to read and faster.
    
    Perf benchmarks from the tubes:
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/ohai/plugins/linux/network.rb
+++ lib/ohai/plugins/linux/network.rb
@@ -141,10 +141,12 @@ Ohai.plugin(:Network) do
   # using a temporary var to hold routes and their interface name
   def parse_routes(family, iface)
     iface.collect do |i, iv|
-      iv[:routes]&.collect do |r|
+      next unless iv[:routes]
+
+      iv[:routes].collect do |r|
         r.merge(dev: i) if r[:family] == family[:name]
-      end&.compact
-    end.compact.flatten
+      end.compact # @todo: when we drop ruby 2.6 this should be a filter_map
+    end.compact.flatten # @todo: when we drop ruby 2.6 this should be a filter_map
   end
 
   # determine layer 1 details for the interface using ethtool

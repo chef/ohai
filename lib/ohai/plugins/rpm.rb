5946a6bac2e99e52473365aa1a51b92197566f25
Davide Cavalca <dcavalca@fb.com>
2021-12-02 08:54:20 -0800

    Fix parsing bug in the rpm plugin
    Signed-off-by: Davide Cavalca <dcavalca@fb.com>

--- lib/ohai/plugins/rpm.rb
+++ lib/ohai/plugins/rpm.rb
@@ -91,16 +91,26 @@ Ohai.plugin(:Rpm) do
       value = ""
       lines[macros_start_idx + 1..macros_end_idx - 1].each do |line|
         if line.start_with?("-")
+          # new macros are always prefixed by a dash
           if name
+            # if we had parsed a macro before, store it
             rpm[:macros][name] = value
             name = nil
             value = ""
-          else
-            _prefix, name, value = line.split(" ", 3)
           end
+
+          # parse the new macro definition
+          _prefix, name, value = line.split(" ", 3)
         else
+          # continuations have no prefix and just append to the previous one
           value += "\n#{line}"
         end
+      end
+
+      # Once we're done parsing all lines, we might have a parsed definition
+      # we haven't stored - if so, store it.
+      if name
+        rpm[:macros][name] = value
       end
     else
       logger.trace("Plugin RPM: Could not find rpm. Skipping plugin.")

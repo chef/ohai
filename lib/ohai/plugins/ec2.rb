dd355653bfd94b40fe79cb45bccd7ea07f2a4272
Greg Batye <gbatye@gmail.com>
2022-09-21 17:36:09 -0700

    Update ec2.rb
    Attempt #2 to fix linters
    
    Signed-off-by: Greg Batye <gbatye@gmail.com>

--- lib/ohai/plugins/ec2.rb
+++ lib/ohai/plugins/ec2.rb
@@ -107,7 +107,6 @@ Ohai.plugin(:EC2) do
   # @return [Boolean] Does the system appear to be on EC2
   def looks_like_ec2?
     return true if hint?("ec2")
-    
     return true if has_ec2_xen_uuid? || has_ec2_amazon_dmi? || has_ec2_xen_dmi? || has_ec2_identifying_number?
   end
 

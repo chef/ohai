60b26f5f4e3a4f4af28241dba0c0829e6798b2c2
Jared Weyer <v-jaredweyer@microsoft.com>
2022-12-13 16:21:49 -0800

    account for situtaion in which parallels is installed on a VM
    Signed-off-by: Jared Weyer <v-jaredweyer@microsoft.com>

--- lib/ohai/plugins/darwin/virtualization.rb
+++ lib/ohai/plugins/darwin/virtualization.rb
@@ -77,17 +77,15 @@ Ohai.plugin(:Virtualization) do
       virtualization[:systems][:vmware] = "guest"
     end
 
-    if prlctl_exists?
+    
+    if ioreg_exists? && /pci1ab8,4000/.match?(shell_out("ioreg -l").stdout)
+      virtualization[:system] = "parallels"
+      virtualization[:role] = "guest"
+      virtualization[:systems][:parallels] = "guest"
+    elsif prlctl_exists? 
       virtualization[:system] = "parallels"
       virtualization[:role] = "host"
       virtualization[:systems][:parallels] = "host"
-    elsif ioreg_exists?
-      so = shell_out("ioreg -l")
-      if /pci1ab8,4000/.match?(so.stdout)
-        virtualization[:system] = "parallels"
-        virtualization[:role] = "guest"
-        virtualization[:systems][:parallels] = "guest"
-      end
     end
   end
 end

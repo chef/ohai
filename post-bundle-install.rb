30ed8625b9f4f883fa68220bad8349c0838c07e8
nikhil2611 <nikhilgupta2102@gmail.com>
2024-10-17 12:57:35 +0530

    fixinf chefstyle changes
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- post-bundle-install.rb
+++ post-bundle-install.rb
@@ -15,9 +15,9 @@ Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
   matches = File.basename(gempath).match(/.*-[A-Fa-f0-9]{12}/)
   next unless matches
 
-  %w[chef-utils chef-config].each do |gem_need_install|
+  %w{chef-utils chef-config}.each do |gem_need_install|
     dir_path = "#{gempath}/#{gem_need_install}"
-    gem_name = File.basename(Dir["#{dir_path}/*.gemspec"].first, ".gemspec") 
+    gem_name = File.basename(Dir["#{dir_path}/*.gemspec"].first, ".gemspec")
     # FIXME: should strip any valid ruby platform off of the gem_name if it matches
 
     next unless gem_name
@@ -25,8 +25,8 @@ Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
     puts "re-installing #{gem_name}..."
 
     Dir.chdir(dir_path) do
-      system("gem build #{gem_name}.gemspec") or raise "gem build failed" 
-      system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed" 
+      system("gem build #{gem_name}.gemspec") or raise "gem build failed"
+      system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed"
     end
   end
 end

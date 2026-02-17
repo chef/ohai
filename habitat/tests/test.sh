d3be1de67174c6ed166f06a6dc9a22a5601004c6
Phil Dibowitz <phil@ipom.com>
2026-02-17 13:54:56 -0800

    Add line-ending lint (#1919)
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- habitat/tests/test.sh
+++ habitat/tests/test.sh
@@ -22,6 +22,3 @@ cd "${project_root}"
 echo "--- :mag_right: Testing ${pkg_ident} executables"
 actual_version=$(hab pkg exec "${pkg_ident}" ohai -v | sed -E 's/.*: ([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
 [[ "$package_version" = "$actual_version" ]] || error "ohai version is not the expected version. Expected '$package_version', got '$actual_version'"
-
-
-
